56414dfc4a50395fe7cb5cd9dc998b10327d9092
FabioSpampinato <spampinabio@gmail.com>
2015-08-01 01:41:58 +0200

    added a flippable component

--- assets/js/min/presto.js
+++ assets/js/min/presto.js
@@ -3073,6 +3073,69 @@ Prism.languages.javascript=Prism.languages.extend("clike",{keyword:/\b(break|cas
 
 
 
+/* FLIPPABLE */
+
+;(function ( $, window, document, undefined ) {
+
+    'use strict';
+
+    /* FLIPPABLE */
+
+    $.widget ( 'presto.flippable', {
+
+        /* OPTIONS */
+
+        options: {
+            callbacks: {
+                flipped: $.noop
+            }
+        },
+
+        /* SPECIAL */
+
+        _variables: function () {
+
+            this.$flippable = this.$element;
+            this.$front = this.$flippable.find ( '.flippable-front' );
+            this.$back = this.$flippable.find ( '.flippable-back' );
+            this.$flippers = this.$flippable.find ( '.flipper' );
+
+            this.isFlipped = this.$flippable.hasClass ( 'flipped' );
+
+        },
+
+        _events: function () {
+
+            this._on ( this.$flippers, 'click', this.flip );
+
+        },
+
+        /* PUBLIC */
+
+        flip: function () {
+
+            this.isFlipped = !this.isFlipped;
+
+            this.$flippable.toggleClass ( 'flipped', this.isFlipped );
+
+            this._trigger ( 'flipped' );
+
+        }
+
+    });
+
+    /* READY */
+
+    $(function () {
+
+        $('.flippable-wrp').flippable ();
+
+    });
+
+}( lQuery, window, document ));
+
+
+
 /* INFOBAR */
 
 ;(function ( $, window, document, undefined ) {
---
+++ assets/js/presto/components/flippable.js
@@ -0,0 +1,61 @@
+
+/* FLIPPABLE */
+
+;(function ( $, window, document, undefined ) {
+
+    'use strict';
+
+    /* FLIPPABLE */
+
+    $.widget ( 'presto.flippable', {
+
+        /* OPTIONS */
+
+        options: {
+            callbacks: {
+                flipped: $.noop
+            }
+        },
+
+        /* SPECIAL */
+
+        _variables: function () {
+
+            this.$flippable = this.$element;
+            this.$front = this.$flippable.find ( '.flippable-front' );
+            this.$back = this.$flippable.find ( '.flippable-back' );
+            this.$flippers = this.$flippable.find ( '.flipper' );
+
+            this.isFlipped = this.$flippable.hasClass ( 'flipped' );
+
+        },
+
+        _events: function () {
+
+            this._on ( this.$flippers, 'click', this.flip );
+
+        },
+
+        /* PUBLIC */
+
+        flip: function () {
+
+            this.isFlipped = !this.isFlipped;
+
+            this.$flippable.toggleClass ( 'flipped', this.isFlipped );
+
+            this._trigger ( 'flipped' );
+
+        }
+
+    });
+
+    /* READY */
+
+    $(function () {
+
+        $('.flippable-wrp').flippable ();
+
+    });
+
+}( lQuery, window, document ));
