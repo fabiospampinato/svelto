8aae18240fb85057dc97cb93e72b4cda86ff850e
FabioSpampinato <spampinabio@gmail.com>
2015-09-05 17:59:38 +0200

    Updated jade mixins headers

---
+++ src/components/core/core.js
@@ -0,0 +1,13 @@
+
+/* =========================================================================
+ * Svelto - @FILE-NAME-UPPERCASED v0.1.0
+ * http://getsvelto.com/@FILE-NAME
+ * =========================================================================
+ * Copyright (c) 2015 Fabio Spampinato
+ * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
+ * =========================================================================
+ * @requires ../extras/lodash-extra.js
+ * @requires ../extras/jQuery-extra.js
+ * @requires ../browser/browser.js
+ * @requires ../ui/ui.js
+ * ========================================================================= */
