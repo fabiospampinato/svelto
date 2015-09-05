62ddced9b091f1a5d0cae3b215df533d1aca4e87
FabioSpampinato <spampinabio@gmail.com>
2015-09-06 01:24:35 +0200

    Improved base widget

--- src/components/widget/base_widget.js
+++ src/components/widget/base_widget.js
@@ -1,6 +1,6 @@
 
 /* =========================================================================
- * Svelto - Base Widget v0.1.0
+ * Svelto - Base Widget v0.2.0
  * =========================================================================
  * Copyright (c) 2015 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
@@ -16,85 +16,82 @@
 
   /* BASE WIDGET */
 
-  $.Widget = function ( /* options, element */ ) {};
+  $.Widget = function () {};
 
   $.Widget._childConstructors = [];
 
+  /* PROTOTYPE */
+
   $.Widget.prototype = {
 
-    /* VARIABLES */
+    /* NAMES */
 
-    widgetOriginalName: 'widget',
+    widgetOriginalName: 'widget', //TODO: Remove if not necessary
     widgetName: 'widget',
     widgetFullName: 'widget',
 
-    defaultElement: false,
-    templates: {}, //INFO: The `base` template will be used as the constructor
+    /* TEMPLATES */
+
+    templates: {
+      base: false //INFO: It will be used as the constructor if no element is provided
+    },
 
     /* OPTIONS */
 
     options: {
-      callbacks: {}
+      selectors: {}, //INFO: Selectors to use inside the widget
+      classes: {}, //INFO: CSS classes to attach inside the widget
+      animations: {}, //INFO: Object storing all the milliseconds required for each animation to occur
+      callbacks: {}, //INFO: Callbacks to trigger on specific events
+      disabled: false //INFO: Determines if the widget is enabled or disabled
     },
 
-    /* WIDGET FUNCTIONS */
+    /* WIDGET METHODS */
 
-    _createWidget: function ( options, element ) {
+    _create: function ( options, element ) {
 
-      // VARIABLES
+      // CHECK IF INITIALIZABLE
 
-      this.initializationType = element
-                      ? 'element'
-                      : this.defaultElement
-                        ? 'html'
-                        : this.templates.base
-                          ? 'template'
-                          : 'none';
+      if ( !element && !this.templates.base ) {
 
-      /* EXTEND OPTIONS */
-
-      this.options = _.merge ( {}, this.options, this._getCreateOptions (), options );
-
-      if ( this.initializationType === 'element' ) {
-
-        _.merge ( this.options, $(element).data ( this.widgetName ) );
+        throw 'WidgetUninitializable';
 
       }
 
+      // MERGE OPTIONS
+
+      this.options = _.merge ( {}, this.options, this._createOptions (), options );
+
       // INIT ELEMENT
 
-      element = $( element || this.defaultElement || ( this.templates.base ? this._tmpl ( 'base', this.options ) : false ) || this ).get ( 0 );
+      this.$element = $(element || this._tmpl ( 'base', this.options ));
+      this.element = this.$element[0];
 
-      this.element = element;
-      this.$element = $(element);
+      // SET GUID
 
-      this.guid = _.uniqueId ();
+      this.guid = $.guid++;
 
-      // DISABLED
+      // SET DISABLED
 
-      this.options.disabled = ( options && options.disabled ) ? options.disabled : this.$element.hasClass ( this.widgetName + '-disabled' );
+      this.options.disabled = this.options.disabled || this.$element.hasClass ( this.widgetName + '-disabled' );
 
-      // SAVING INSTANCE
+      // SAVE WIDGET INSTANCE
 
       $.data ( this.element, this.widgetFullName, this );
 
       // ON $ELEMENT REMOVE -> WIDGET DESTROY
 
-      this._on ( true, this.$element, 'remove', function ( event ) {
+      this._on ( true, 'remove', function ( event ) {
 
         if ( event.target === this.element ) {
 
-          this.destroy ();
+          this.destroy ( event );
 
         }
 
       });
 
-      /* CALLBACKS */
-
-      this._create ();
-
-      this._trigger ( 'create', this._getCreateEventData () );
+      // CALLBACKS
 
       this._variables ();
 
@@ -104,12 +101,11 @@
 
     },
 
-    _getCreateOptions: _.noop,
-    _getCreateEventData: _.noop,
-    _create: _.noop,
-    _variables: _.noop,
-    _init: _.noop,
-    _events: _.noop,
+    _createOptions: _.noop, //INFO: Returns an options object that will be used for the current widget instance, generated during widget instantiation
+
+    _variables: _.noop, //INFO: Init your variables inside this function
+    _init: _.noop, //INFO: Perform the init stuff inside this function
+    _events: _.noop, //INFO: Bind the event handlers inside this function
 
     destroy: function () {
 
@@ -127,34 +123,28 @@
 
     },
 
-    /* OPTIONS FUNCTIONS */
+    /* OPTIONS METHODS */
 
     option: function ( key, value ) {
 
-      if ( arguments.length === 0 ) {
+      if ( arguments.length === 0 ) { //INFO: Returns a clone of the options object
 
         return _.cloneDeep ( this.options );
 
       }
 
-      var options = key,
-        parts,
-        curOption,
-        i;
+      if ( _.isString ( key ) ) { //INFO: Handle nested keys, for example: 'foo.bar' => { foo: { bar: '' } }
 
-      if ( typeof key === 'string' ) {
+        var options = {},
+            parts = key.split ( '.' );
 
-        // handle nested keys, e.g., "foo.bar" => { foo: { bar: ___ } }
-
-        options = {};
-        parts = key.split ( '.' );
         key = parts.shift ();
 
         if ( parts.length ) {
 
-          curOption = options[key] = _.extend ( {}, this.options[key] );
+          var curOption = options[key] = _.extend ( {}, this.options[key] );
 
-          for ( i = 0; i < parts.length - 1; i++ ) {
+          for ( var i = 0; i < parts.length - 1; i++ ) {
 
             curOption[parts[i]] = curOption[parts[i]] || {};
             curOption = curOption[parts[i]];
@@ -165,17 +155,17 @@
 
           if ( arguments.length === 1 ) {
 
-            return curOption[key] === undefined ? null : curOption[key];
+            return _.isUndefined ( curOption[key] ) ? null : curOption[key];
 
           }
 
           curOption[key] = value;
 
-        } else {
+        } else { //INFO: Handle single level property
 
           if ( arguments.length === 1 ) {
 
-            return this.options[key] === undefined ? null : this.options[key];
+            return _.isUndefined ( this.options[key] ) ? null : this.options[key];
 
           }
 
@@ -183,9 +173,11 @@
 
         }
 
-      }
+      } else if ( _.isPlainObject ( key ) ) { //INFO: Set multiple properties
 
-      this._setOptions ( options );
+        this._setOptions ( key );
+
+      }
 
       return this;
 
@@ -217,13 +209,15 @@
 
     },
 
-    /* ENABLING / DISABLING */
+    /* ENABLING */
 
     enable: function () {
 
       return this._setOptions ({ disabled: false });
 
     },
+
+    /* DISABLING */
 
     disable: function () {
 
@@ -237,9 +231,13 @@
 
       //TODO: Add support for custom data
 
+      // SAVE A REFERENCE TO THIS
+
       var instance = this;
 
-      if ( typeof suppressDisabledCheck !== 'boolean' ) {
+      // NORMALIZING OPTIONS
+
+      if ( !_.isBoolean ( suppressDisabledCheck ) ) {
 
         handler = selector;
         selector = events;
@@ -265,11 +263,15 @@
 
       }
 
+      // SUPPORT FOR STRING HANDLERS REFERRING TO A SELF METHOD
+
       handler = _.isString ( handler ) ? this[handler] : handler;
+
+      // PROXY
 
       function handlerProxy () {
 
-        if ( !suppressDisabledCheck && ( instance.options.disabled || instance.$element.hasClass ( instance.widgetName + '-disabled' ) ) ) return;
+        if ( !suppressDisabledCheck && instance.options.disabled ) return;
 
         var args = _.slice ( arguments );
 
@@ -279,21 +281,29 @@
 
       }
 
+      // PROXY GUID
+
       handlerProxy.guid = handler.guid = ( handler.guid || handlerProxy.guid || $.guid++ );
 
-      if ( selector ) {
+      // TRIGGERING
+
+      if ( selector ) { // DELEGATED
 
         $element.on ( events, selector, handlerProxy );
 
-      } else {
+      } else { // NORMAL
 
         $element.on ( events, handlerProxy );
 
       }
 
+      return this;
+
     },
 
     _off: function ( $element, events, handler ) {
+
+      // NORMALIZING OPTIONS
 
       if ( !handler ) {
 
@@ -303,9 +313,15 @@
 
       }
 
+      // SUPPORT FOR STRING HANDLERS REFERRING TO A SELF METHOD
+
       handler = _.isString ( handler ) ? this[handler] : handler;
 
+      // REMOVING HANDLER
+
       $element.off ( events, handler );
+
+      return this;
 
     },
 
@@ -319,7 +335,7 @@
 
         this.$element.trigger ( this.widgetName + ':' + events[ei], data );
 
-        if ( typeof this.options.callbacks[events[ei]] === 'function' ) {
+        if ( _.isFunction ( this.options.callbacks[events[ei]] ) ) {
 
           this.options.callbacks[events[ei]].call ( this.element, data );
 
@@ -327,19 +343,45 @@
 
       }
 
+      return this;
+
     },
 
     /* DELAYING */
 
-    _delay: function ( handler, delay ) {
+    _delay: function ( fn, delay ) {
 
       var instance = this;
 
       return setTimeout ( function () {
 
-        handler.apply ( instance, arguments );
+        fn.apply ( instance, arguments );
 
       }, delay || 0 );
+
+    },
+
+    /* DEFER */
+
+    _defer: function ( fn ) {
+
+      return this._delay ( fn );
+
+    },
+
+    /* DEBOUNCING */
+
+    _debounce: function ( fn, wait, options ) { //TODO: Test it, expecially regarding the `this` variable
+
+      return _.debounce ( fn, wait, options );
+
+    },
+
+    /* THROTTLING */
+
+    _throttle: function ( fn, wait, options ) { //TODO: Test it, expecially regarding the `this` variable
+
+      return _.throttle ( fn, wait, options );
 
     },
 
@@ -347,11 +389,11 @@
 
     _tmpl: function ( name, options ) {
 
-      return $.tmpl ( this.widgetOriginalName + '.' + name, options );
+      return $.tmpl ( this.widgetOriginalName + '.' + name, options || {} );
 
     },
 
-    /* MANIPULATION */
+    /* INSERTION */
 
     insertBefore: function ( selector ) {
 
