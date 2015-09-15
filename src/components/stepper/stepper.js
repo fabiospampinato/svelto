f1c81615f6ff3aa0f128b54173895204062e2212
FabioSpampinato <spampinabio@gmail.com>
2015-09-15 02:03:18 +0200

    Finalized stepper

--- src/components/stepper/stepper.js
+++ src/components/stepper/stepper.js
@@ -1,6 +1,6 @@
 
 /* =========================================================================
- * Svelto - Stepper v0.1.0
+ * Svelto - Stepper v0.2.0
  * =========================================================================
  * Copyright (c) 2015 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
@@ -23,8 +23,13 @@
       max: 100,
       value: 0,
       step: 1,
-      decimals: 0,
+      selectors: {
+        decreaser: '.stepper-decreaser',
+        input: 'input',
+        increaser: '.stepper-increaser'
+      },
       callbacks: {
+        change: _.noop,
         increase: _.noop,
         decrease: _.noop
       }
@@ -35,9 +40,17 @@
     _variables: function () {
 
       this.$stepper = this.$element;
-      this.$input = this.$stepper.find ( 'input' );
-      this.$decreaser = this.$stepper.find ( '.stepper-decreaser' );
-      this.$increaser = this.$stepper.find ( '.stepper-increaser' );
+      this.$decreaser = this.$stepper.find ( this.options.selectors.decreaser );
+      this.$input = this.$stepper.find ( this.options.selectors.input );
+      this.$increaser = this.$stepper.find ( this.options.selectors.increaser );
+
+      this.options.value = this._sanitizeValue ( this.options.value );
+
+    },
+
+    _init: function () {
+
+      this._updateButtons ();
 
     },
 
@@ -45,60 +58,104 @@
 
       /* INPUT / CHANGE */
 
-      this._on ( true, this.$input, 'input change', this._handler_input_change );
+      this._on ( true, this.$input, 'input change', this.__inputChange );
 
       /* ARROWS */
 
-      this._on ( 'mouseenter', this._handler_arrows_in );
-      this._on ( 'mouseleave', this._handler_arrows_out );
+      this._on ( 'mouseenter', this.__arrowsIn );
+      this._on ( 'mouseleave', this.__arrowsOut );
 
-      /* INCREASE / DECREASE */
+      /* INCREASE */
 
-      this._on ( this.$decreaser, 'click', this.decrease );
+      this._on ( this.$decreaser, $.Pointer.tap, this.decrease );
 
-      this._on ( this.$increaser, 'click', this.increase );
+      /* DECREASE */
+
+      this._on ( this.$increaser, $.Pointer.tap, this.increase );
 
     },
 
     /* PRIVATE */
 
-    _round_value: function ( value ) {
+    _sanitizeValue: function ( value ) {
 
-      return Number(Number(value).toFixed ( this.options.decimals ));
+      var nr = Number ( value );
+
+      value = ( _.isNaN ( nr ) ? 0 : nr );
+
+      var remaining = ( value % this.options.step );
+
+      value = value - remaining + ( remaining >= this.options.step / 2 ? this.options.step : 0 );
+
+      return _.clamp ( this.options.min, value, this.options.max );
+
+    },
+
+    /* UPDATE */
+
+    _updateInput: function () {
+
+      this.$input.val ( this.options.value ).trigger ( 'change' );
+
+    },
+
+    _updateButtons: function ( previous ) {
+
+      var isMin = ( this.options.value === this.options.min ),
+          isMax = ( this.options.value === this.options.max );
+
+      if ( previous === this.options.min || isMin ) {
+
+        this.$decreaser.toggleClass ( 'disabled', isMin );
+
+      } else if ( previous === this.options.max || isMax ) {
+
+        this.$increaser.toggleClass ( 'disabled', isMax );
+
+      }
+
+    },
+
+    _update: function ( previous ) {
+
+      this._updateInput ();
+      this._updateButtons ( previous );
 
     },
 
     /* CHANGE */
 
-    _handler_input_change: function () {
+    __inputChange: function () {
 
-      this.set_value ( this.$input.val () );
+      this.set ( this.$input.val () );
 
     },
 
     /* LEFT / RIGHT ARROWS */
 
-    _handler_arrows_in: function ( event ) {
+    __arrowsIn: function ( event ) {
 
-      this._on ( $document, 'keydown', this._handler_arrows_keydown );
-
-    },
-
-    _handler_arrows_out: function ( event ) {
-
-      this._off ( $document, 'keydown', this._handler_arrows_keydown );
+      this._on ( $document, 'keydown', this.__keydown );
 
     },
 
-    _handler_arrows_keydown: function ( event ) {
+    __arrowsOut: function ( event ) {
 
-      if ( event.keyCode === $.ui.keyCode.LEFT || event.keyCode === $.ui.keyCode.DOWN ) {
+      this._off ( $document, 'keydown', this.__keydown );
 
-        this.decrease ();
+    },
 
-      } else if ( event.keyCode === $.ui.keyCode.RIGHT || event.keyCode === $.ui.keyCode.UP ) {
+    __keydown: function ( event ) {
 
-        this.increase ();
+      switch ( event.keyCode ) {
+
+        case $.ui.keyCode.UP:
+          this.increase ();
+          break;
+
+        case $.ui.keyCode.DOWN:
+          this.decrease ();
+          break;
 
       }
 
@@ -106,22 +163,46 @@
 
     /* PUBLIC */
 
-    set_value: function ( value ) {
+    get: function () {
 
-      value = this._round_value ( value );
+      return this.options.value;
 
-      if ( value !== this.options.value || this.$input.val ().length === 0 ) {
+    },
 
-        var clamped = _.clamp ( this.options.min, value, this.options.max );
+    set: function ( value ) {
 
-        this.options.value = clamped;
+      value = Number ( value );
 
-        this.$input.val ( clamped ).trigger ( 'change' );
+      if ( !_.isNaN ( value ) ) {
 
-        this.$decreaser.toggleClass ( 'disabled', clamped === this.options.min );
-        this.$increaser.toggleClass ( 'disabled', clamped === this.options.max );
+        value = this._sanitizeValue ( value );
 
-        this._trigger ( clamped > this.options.value ? 'increase' : 'decrease' );
+        if ( value !== this.options.value ) {
+
+          var data = {
+            previous: this.options.value,
+            value: value
+          };
+
+          this.options.value = value;
+
+          this._update ( data.previous );
+
+          this._trigger ( 'change', data );
+
+          this._trigger ( ( data.previous < data.value ) ? 'increase' : 'decrease', data );
+
+          return;
+
+        }
+
+      }
+
+      /* RESETTING IF WE ALTERED THE INPUT VALUE */
+
+      if ( this.$input.val () !== String ( this.options.value ) ) {
+
+        this._updateInput ();
 
       }
 
@@ -129,21 +210,13 @@
 
     increase: function () {
 
-      this.navigate ( this.options.step );
+      this.set ( this.options.value + this.options.step );
 
     },
 
     decrease: function () {
 
-      this.navigate ( - this.options.step );
-
-    },
-
-    navigate: function ( modifier ) {
-
-      var new_value = this.options.value + modifier;
-
-      this.set_value ( new_value );
+      this.set ( this.options.value - this.options.step );
 
     }
 
@@ -155,19 +228,14 @@
 
     $('.stepper').each ( function () {
 
-      return;
+      var $stepper = $(this);
 
-      var $stepper = $(this),
-        $input = $stepper.find ( 'input' ),
-        options = {
-          min: Number($stepper.data ( 'min' ) || 0),
-          max: Number($stepper.data ( 'max' ) || 100),
-          value: Number($input.val () || 0),
-          step: Number($stepper.data ( 'step' ) || 1),
-          decimals: Number($stepper.data ( 'decimals' ) || 0)
-        };
-
-      $stepper.stepper ( options );
+      $stepper.stepper ({
+        min: Number($stepper.data ( 'min' ) || 0),
+        max: Number($stepper.data ( 'max' ) || 100),
+        value: Number($stepper.find ( '.stepper-input' ).val () || 0),
+        step: Number($stepper.data ( 'step' ) || 1)
+      });
 
     });
 
