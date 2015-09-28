a02b72d0a120d6754e8dec9b4c33ad2ab7b20e90
FabioSpampinato <spampinabio@gmail.com>
2015-09-28 22:57:39 +0200

    Finalized tabs component

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,6 +1,6 @@
 
 /* =========================================================================
- * Svelto - Tabs v0.1.0
+ * Svelto - Tabs v0.2.0
  * =========================================================================
  * Copyright (c) 2015 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
@@ -8,8 +8,7 @@
  * @requires ../widget/factory.js
  * ========================================================================= */
 
-//TODO: Maybe switch from the indicator to .button.highlight
-//FIXME: positionate_indicator is too hacky
+//TODO: Add again the indicator
 
 ;(function ( $, _, window, document, undefined ) {
 
@@ -22,18 +21,20 @@
     /* OPTIONS */
 
     options: {
-      selectors: {
-        buttons_wrp: '.tabs-buttons',
-        buttons: '.button-wrp > .label',
-        button_active_class: 'active',
-        indicator: '.tabs-buttons-indicator',
-        containers_wrp: '.tabs-containers',
-        containers: '> .container',
-        container_active_class: 'active'
+      highlight: true,
+      classes: {
+        vertical: 'vertical',
+        active: {
+          trigger: 'active',
+          container: 'active'
+        }
       },
-      indicator_delay: 40,
+      selectors: {
+        triggers: '.tabs-triggers > *',
+        containers: '.tabs-containers > *'
+      },
       callbacks: {
-        select: _.noop
+        set: _.noop
       }
     },
 
@@ -42,118 +43,92 @@
     _variables: function () {
 
       this.$tabs = this.$element;
+      this.$triggers = this.$tabs.find ( this.options.selectors.triggers );
+      this.$containers = this.$tabs.find ( this.options.selectors.containers );
 
-      this.isVertical = this.$tabs.hasClass ( 'vertical' );
+      this.isVertical = this.$tabs.hasClass ( this.options.classes.vertical );
 
-      this.$tabs_buttons = this.$tabs.find ( this.options.selectors.buttons_wrp );
-      this.$buttons = this.$tabs_buttons.find ( this.options.selectors.buttons );
-      this.$indicator = this.$tabs.find ( this.options.selectors.indicator );
-
-      this.$tabs_containers = this.$tabs.find ( this.options.selectors.containers_wrp );
-      this.$containers = this.$tabs_containers.find ( this.options.selectors.containers );
-
-      var $current_button = this.$buttons.filter ( '.' + this.options.selectors.button_active_class ).first ();
-
-      $current_button = ( $current_button.length > 0 ) ? $current_button : this.$buttons.first ();
-
-      this.prev_index = 0;
-      this.current_index = this.$buttons.index ( $current_button );
+      this.index = -1;
 
     },
 
     _init: function () {
 
-      this.select ( this.current_index, true );
+      var $activeTrigger = this.$triggers.filter ( '.' + this.options.classes.active.trigger ).first ();
+
+      $activeTrigger = ( $activeTrigger.length > 0 ) ? $activeTrigger : this.$triggers.first ();
+
+      var newIndex = this.$triggers.index ( $activeTrigger );
+
+      this.set ( newIndex );
 
     },
 
     _events: function () {
 
-      this._on ( this.$tabs_buttons, 'click', this.options.selectors.buttons, this._hander_button_click );
+      /* TRIGGERS */
 
-      this._on ( $window, 'resize', this._positionate_indicator ); //TODO: throttle or devounce it
+      this._on ( this.$triggers, $.Pointer.tap, this.__triggerClick );
 
     },
 
     /* PRIVATE */
 
-    _hander_button_click: function ( event, node ) {
+    __triggerClick: function ( event, node ) {
 
-      var new_index = this.$buttons.index ( $(node) );
+      var newIndex = this.$triggers.index ( $(node) );
 
-      this.select ( new_index );
-
-    },
-
-    _positionate_indicator: function () {
-
-      var $active = this.$buttons.filter ( '.' + this.options.selectors.button_active_class ),
-        position = $active.position ();
-
-      if ( this.isVertical ) {
-
-        var total_height = this.$tabs_buttons.height ();
-
-        this._delay ( function () {
-
-          var top = position.top + ( this.$buttons.index ( $active ) === 0 ? 1 : 0 ); //FIXME: it's hacky
-
-          this.$indicator.css ( 'top', ( top * 100 / total_height ) + '%' );
-
-        }, this.current_index > this.prev_index ? this.options.indicator_delay : 0 );
-
-        this._delay ( function () {
-
-          var bottom = total_height - position.top - $active.height () + ( this.$buttons.index ( $active ) === this.$buttons.length - 1 ? 1 : 0 ); //FIXME: it's hacky
-
-          this.$indicator.css ( 'bottom', ( bottom * 100 / total_height ) + '%' );
-
-        }, this.current_index > this.prev_index ? 0 : this.options.indicator_delay );
-
-      } else {
-
-        var total_width = this.$tabs_buttons.width ();
-
-        this._delay ( function () {
-
-          var left = position.left + ( this.$buttons.index ( $active ) === 0 ? 1 : 0 ); //FIXME: it's hacky
-
-          this.$indicator.css ( 'left', ( left * 100 / total_width ) + '%' );
-
-        }, this.current_index > this.prev_index ? this.options.indicator_delay : 0 );
-
-        this._delay ( function () {
-
-          var right = total_width - position.left - $active.width () + ( this.$buttons.index ( $active ) === this.$buttons.length - 1 ? 1 : 0 ); //FIXME: it's hacky
-
-          this.$indicator.css ( 'right', ( right * 100 / total_width ) + '%' );
-
-        }, this.current_index > this.prev_index ? 0 : this.options.indicator_delay );
-
-      }
+      this.set ( newIndex );
 
     },
 
     /* PUBLIC */
 
-    select: function ( index, force ) {
+    get: function () {
 
-      if ( this.current_index !== index || force ) {
+      return this.index;
 
-        this.$buttons.eq ( this.current_index ).removeClass ( this.options.selectors.button_active_class );
-        this.$buttons.eq ( index ).addClass ( this.options.selectors.button_active_class );
+    },
 
-        this.$containers.eq ( this.current_index ).removeClass ( this.options.selectors.container_active_class );
-        this.$containers.eq ( index ).addClass ( this.options.selectors.container_active_class );
+    set: function ( index ) {
 
-        if ( this.current_index !== index ) {
+      if ( this.index !== index ) {
 
-          this.prev_index = this.current_index;
-          this.current_index = index;
+        /* PREVIOUS */
+
+        var $prevTrigger = this.$triggers.eq ( this.index ),
+            $prevContainer = this.$containers.eq ( this.index );
+
+        $prevTrigger.removeClass ( this.options.classes.active.trigger );
+        $prevContainer.removeClass ( this.options.classes.active.container );
+
+        if ( this.options.highlight ) {
+
+          $prevTrigger.removeClass ( 'highlight highlight-bottom highlight-right' );
 
         }
 
-        this._positionate_indicator ();
+        /* NEW */
+
+        this.index = index;
+
+        var $trigger = this.$triggers.eq ( this.index ),
+            $container = this.$containers.eq ( this.index );
+
+        $trigger.addClass ( this.options.classes.active.trigger );
+        $container.addClass ( this.options.classes.active.container );
+
+        if ( this.options.highlight ) {
+
+          $trigger.addClass ( 'highlight' + ( this.isVertical ? ' highlight-right' : ' highlight-bottom' ) );
+
+        }
+
+        /* CALLBACKS */
+
+        this._trigger ( 'set', {
+          index: this.index
+        });
 
       }
 
