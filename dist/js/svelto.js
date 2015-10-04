115c7266a46e55b94c652e8b29faf9e7026a4795
FabioSpampinato <spampinabio@gmail.com>
2015-10-04 23:47:17 +0200

    Finalized sortable

--- dist/js/svelto.js
+++ dist/js/svelto.js
@@ -7471,7 +7471,7 @@ $(function () {
 
 
 /* =========================================================================
- * Svelto - Sortable v0.1.0
+ * Svelto - Sortable v0.2.0
  * =========================================================================
  * Copyright (c) 2015 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
@@ -7479,10 +7479,9 @@ $(function () {
  * @requires ../widget/factory.js
  * ========================================================================= */
 
-//TODO: add support for tableHelper, just put the new addded row in the right position, good performance gain here!
-//TODO: cache the column datas, if possible
-//TODO: add support for sorting other things other than tables
-//TODO: if possible sort using flexbox's `order` property
+//TODO: Add support for tableHelper, just put the new addded row in the right position, good performance gain here!
+//TODO: Add support for sorting other things other than tables
+//TODO: If possible sort using flexbox's `order` property
 
 ;(function ( $, _, window, document, undefined ) {
 
@@ -7508,6 +7507,20 @@ $(function () {
           return a.localeCompare ( b );
         }
       },
+      sortValue: 'sort-value',
+      classes: {
+        sort: {
+          asc: 'sort-asc',
+          desc: 'sort-desc'
+        }
+      },
+      selectors: {
+        header: 'thead th',
+        sortable: '[data-sort]',
+        body: 'tbody',
+        notEmptyRow: 'tr:not(.table-row-empty)',
+        rowCell: 'td'
+      },
       callbacks: {
         sort: _.noop
       }
@@ -7518,27 +7531,29 @@ $(function () {
     _variables: function () {
 
       this.$table = this.$element;
-      this.$headers = this.$table.find ( 'thead th' );
-      this.$sortables = this.$headers.filter ( '[data-sort]' );
-      this.$tbody = this.$table.find ( 'tbody' );
+      this.$headers = this.$table.find ( this.options.selectors.header );
+      this.$sortables = this.$headers.filter ( this.options.selectors.sortable );
+      this.$tbody = this.$table.find ( this.options.selectors.body );
 
       this.table = this.element;
-      this.tbody = this.$tbody.get ( 0 );
+      this.tbody = this.$tbody[0];
 
-      this.sort_datas = {}; //INFO: Caching object for datas and references to rows
+      this.sortData = {}; //INFO: Caching object for datas and references to rows
+      this.updated = false;
 
-      this.current_index = false; //INFO: `$headers` index, not `$sortables` index
-      this.current_direction = false;;
+      this.$currentSortable = false;
+      this.currentIndex = false; //INFO: `$headers` index, not `$sortables` index
+      this.currentDirection = false;
 
     },
 
     _init: function () {
 
-      var $initial = this.$headers.filter ( '.asc, .desc' ).first ();
+      var $initial = this.$headers.filter ( '.' + this.options.classes.sort.asc + ', .' + this.options.classes.sort.desc ).first ();
 
-      if ( $initial.length ) {
+      if ( $initial.length === 1 ) {
 
-        this.sort ( this.$headers.index ( $initial ), ( $initial.hasClass ( 'asc' ) ? 'asc' : 'desc' ) );
+        this.sort ( this.$headers.index ( $initial ), ( $initial.hasClass ( this.options.classes.sort.asc ) ? 'asc' : 'desc' ) );
 
       }
 
@@ -7546,21 +7561,26 @@ $(function () {
 
     _events: function () {
 
-      this._on ( true, 'change', this._handler_change ); //TODO: update to support tableHelper
+      /* CHANGE */
 
-      this._on ( this.$sortables, 'click', this._handler_click );
+      this._on ( true, 'change', this.__change ); //TODO: Update to support tableHelper
+
+      /* CLICK */
+
+      this._on ( this.$sortables, Pointer.tap, this.__click );
 
     },
 
     /* CHANGE */
 
-    _handler_change: function () {
+    __change: function () {
 
-      if ( this.current_index ) {
+      if ( this.currentIndex !== false ) {
 
-        this.sort_datas = {};
+        this.sortData = {};
+        this.updated = false;
 
-        this.sort ( this.current_index, this.current_direction );
+        this.sort ( this.currentIndex, this.currentDirection );
 
       }
 
@@ -7568,16 +7588,16 @@ $(function () {
 
     /* CLICK */
 
-    _handler_click: function ( event ) {
+    __click: function ( event ) {
 
-      var new_index = this.$headers.index ( event.target ),
-        new_direction = this.current_index === new_index
-                  ? this.current_direction === 'asc'
-                    ? 'desc'
-                    : 'asc'
-                  : 'asc';
+      var newIndex = this.$headers.index ( event.target ),
+          newDirection = this.currentIndex === newIndex
+                           ? this.currentDirection === 'asc'
+                             ? 'desc'
+                             : 'asc'
+                           : 'asc';
 
-      this.sort ( new_index, new_direction );
+      this.sort ( newIndex, newDirection );
 
     },
 
@@ -7585,87 +7605,118 @@ $(function () {
 
     sort: function ( index, direction ) {
 
-      // VALIDATE
+      /* VALIDATE */
 
       var $sortable = this.$headers.eq ( index );
 
-      if ( !$sortable.length ) return; // bad index
+      if ( !$sortable.length ) return; //INFO: Bad index
 
-      var sorter_name = $sortable.data ( 'sort' );
+      var sorterName = $sortable.data ( 'sort' );
 
-      if ( !sorter_name ) return; // unsortable column
+      if ( !sorterName ) return; //INFO: Unsortable column
 
-      var sorter = this.options.sorters[sorter_name];
+      var sorter = this.options.sorters[sorterName];
 
-      if ( !sorter ) return; // unsupported sorter
+      if ( !sorter ) return; //INFO: Unsupported sorter
 
       direction = ( direction && direction.toLowerCase () === 'desc' ) ? 'desc' : 'asc';
 
-      // STYLE
+      /* CHECKING CACHED DATAS */
 
-      if ( this.current_index !== false ) {
+      if ( _.isUndefined ( this.sortData[index] ) || !this.updated ) {
 
-        this.$sortables.eq ( this.current_index ).removeClass ( this.current_direction );
+        /* VARIABLES */
 
-      }
+        var $trs = this.$tbody.find ( this.options.selectors.notEmptyRow );
 
-      $sortable.addClass ( direction );
+        this.sortData[index] = Array ( $trs.length );
 
-      // CHECKING CACHED DATAS
-
-      if ( _.isUndefined ( this.sort_datas[index] ) ) {
-
-        // VARIABLES
-
-        var $trs = this.$tbody.find ( 'tr:not(.empty)' );
-
-        this.sort_datas[index] = Array ( $trs.length );
-
-        // POPULATE
+        /* POPULATE */
 
         for ( var i = 0, l = $trs.length; i < l; i++ ) {
 
-          var $td = $trs.eq ( i ) .find ( 'td' ).eq ( index ),
-            value = $td.data ( 'sort-value' ) || $td.text ();
+          var $td = $trs.eq ( i ).find ( this.options.selectors.rowCell ).eq ( index ),
+              value = $td.data ( this.options.sortValue ) || $td.text ();
 
-          this.sort_datas[index][i] = [$trs.get ( i ), value];
+          this.sortData[index][i] = [$trs[i], value];
 
         }
 
       }
 
-      // SORT
+      /* SORT */
 
-      this.sort_datas[index].sort ( function ( a, b ) {
+      if ( index !== this.currentIndex || !this.updated ) {
 
-        return sorter ( a[1], b[1] );
+        this.sortData[index].sort ( function ( a, b ) {
 
-      });
+          return sorter ( a[1], b[1] );
 
-      if ( direction === 'desc' ) this.sort_datas[index].reverse ();
-
-      // APPEND
-
-      this.table.removeChild ( this.tbody ); // detach
-
-      for ( var i = 0, l = this.sort_datas[index].length; i < l; i++ ) {
-
-        this.tbody.appendChild ( this.sort_datas[index][i][0] ); // reorder
+        });
 
       }
 
-      this.table.appendChild ( this.tbody ); // attach
+      /* REVERSING */
 
-      // UPDATE
+      if ( this.updated && index === this.currentIndex && this.currentDirection !== false  ) {
 
-      this.current_index = index;
-      this.current_direction = direction;
+        var needReversing = ( direction !== this.currentDirection );
 
-      // TRIGGER
+      } else {
+
+        var needReversing = ( direction === 'desc' );
+
+      }
+
+      if ( needReversing ) {
+
+        this.sortData[index].reverse ();
+
+      }
+
+      /* REORDER */
+
+      if ( index !== this.currentIndex || direction !== this.currentDirection || !this.updated ) {
+
+        this.table.removeChild ( this.tbody ); //INFO: Detach
+
+        for ( var i = 0, l = this.sortData[index].length; i < l; i++ ) {
+
+          this.tbody.appendChild ( this.sortData[index][i][0] ); //INFO: Reorder
+
+        }
+
+        this.table.appendChild ( this.tbody ); //INFO: Attach
+
+      }
+
+      /* STYLE */
+
+      if ( index !== this.currentIndex || direction !== this.currentDirection ) {
+
+        if ( this.$currentSortable ) {
+
+          this.$currentSortable.removeClass ( this.options.classes.sort[this.currentDirection] );
+
+        }
+
+        $sortable.addClass ( this.options.classes.sort[direction] );
+
+      }
+
+      /* UPDATE */
+
+      this.updated = true;
+
+      this.$currentSortable = $sortable;
+      this.currentIndex = index;
+      this.currentDirection = direction;
+
+      /* TRIGGER */
 
       this._trigger ( 'sort', {
-        index: this.current_index,
-        direction: this.current_direction
+        index: this.currentIndex,
+        direction: this.currentDirection
       });
 
     }
@@ -7676,7 +7727,7 @@ $(function () {
 
   $(function () {
 
-    $('table.sortable').sortable ();
+    $('table.table.sortable').sortable ();
 
   });
 
