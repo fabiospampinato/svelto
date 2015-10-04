11f42809162734ee6b444388239f2c9b8b4fcb81
FabioSpampinato <spampinabio@gmail.com>
2015-10-04 15:04:21 +0200

    Finalized tableHelper

--- dist/js/svelto.js
+++ dist/js/svelto.js
@@ -8186,7 +8186,7 @@ $(function () {
 
 
 /* =========================================================================
- * Svelto - Table Helper v0.1.0
+ * Svelto - Table Helper v0.2.0
  * =========================================================================
  * Copyright (c) 2015 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
@@ -8202,9 +8202,33 @@ $(function () {
 
   $.factory ( 'svelto.tableHelper', {
 
+    /* TEMPLATES */
+
+    templates: {
+      row: '<tr {%= ( o.id ? "class=" + o.id : "" ) %} >' +
+             '{% for ( var i = 0, l = o.datas.length; i < l; i++ ) { %}' +
+               '<td>' +
+                 '{%=o.datas[i]%}' +
+               '</td>' +
+             '{% } %}' +
+             '{% for ( var i = 0, l = o.missing; i < l; i++ ) { %}' +
+               '<td></td>' +
+             '{% } %}' +
+           '</tr>'
+    },
+
     /* OPTIONS */
 
     options: {
+      rowIdPrefix: 'rid',
+      selectors: {
+        header: 'thead',
+        body: 'tbody',
+        headerCell: 'th',
+        rowCell: 'td',
+        emptyRow: 'tr.table-row-empty',
+        notEmptyRow: 'tr:not(.table-row-empty)'
+      },
       callbacks: {
         add: _.noop,
         update: _.noop,
@@ -8218,32 +8242,34 @@ $(function () {
     _variables: function () {
 
       this.$table = this.$element;
-      this.$thead = this.$table.find ( 'thead' ),
-      this.$tfoot = this.$table.find ( 'tfoot' ),
-      this.$tbody = this.$table.find ( 'tbody' ),
-      this.$headers = this.$thead.find ( 'th' ),
-      this.$empty_row = this.$tbody.find ( 'tr.table-row-empty' ),
-      this.columns_nr = this.$headers.length;
+      this.$header = this.$table.find ( this.options.selectors.header );
+      this.$body = this.$table.find ( this.options.selectors.body );
+      this.$headerCells = this.$header.find ( this.options.selectors.headerCell );
+      this.$emptyRow = this.$body.find ( this.options.selectors.emptyRow );
+
+      this.columnsNr = this.$headerCells.length;
 
     },
 
     _init: function () {
 
-      this._check_empty ();
+      this._checkEmpty ();
 
     },
 
     /* PRIVATE */
 
-    _check_empty: function () {
+    _checkEmpty: function () {
 
-      this.$empty_row.toggleClass ( 'hidden', this.$tbody.find ( 'tr:not(.table-row-empty)' ).length > 0 );
+      var hasNonEmptyRows = this.$body.find ( this.options.selectors.notEmptyRow ).length > 0;
+
+      this.$emptyRow.toggleClass ( 'hidden', hasNonEmptyRows );
 
     },
 
-    _get_row_id: function ( id ) {
+    _getRowId: function ( id ) {
 
-      return 'rid_' + this.uuid + '_' + id;
+      return this.options.rowIdPrefix + '_' + this.guid + '_' + id;
 
     },
 
@@ -8251,64 +8277,34 @@ $(function () {
 
     add: function ( id ) { //INFO: id, datas...
 
-      var datas = _.tail ( arguments );
+      var datas = _.tail ( arguments ),
+          rowId = id ? this._getRowId ( id ) : false;
 
-      if ( datas.length ) {
+      if ( datas.length > 0 ) {
 
-        var $fillables = this.$tbody.find ( 'td.fillable' );
+        if ( rowId && $( '.' + rowId ).length === 1 ) return this;
 
-        if ( $fillables.length ) {
-
-          var datas_fillable = _.slice ( datas, 0, $fillables.length );
-
-          datas = _.slice ( datas, datas_fillable.length - 1, datas.length );
-
-          for ( var i = 0, l = datas_fillable.length; i < l; i++ ) {
-
-            $fillables.eq ( i ).html ( datas_fillable[i] ).removeClass ( 'fillable' );
-
-          }
-
-        }
-
-        if ( id && $( '.' + this._get_row_id ( id ) ).length ) {
-
-          $.noty ( 'A table cannot contain 2 rows with the same ID' );
-          return this;
-
-        };
-
-        var chunks = _.chunk ( datas, this.columns_nr );
+        var chunks = _.chunk ( datas, this.columnsNr ),
+            $rows = $empty;
 
         for ( var ci = 0, cl = chunks.length; ci < cl; ci++ ) {
 
-          var chunk = chunks[ci];
+          var chunk = chunks[ci],
+              rowHtml = this._tmpl ( 'row', { id: rowId, datas: chunk, missing: this.columnsNr - chunk.length } );
 
-          var row_html = '<tr ' + ( id ? 'class="' + this._get_row_id ( id ) + '"' : '' ) + '>';
-
-          for ( var i = 0, l = chunk.length; i < l; i++ ) {
-
-            row_html += '<td>' + ( chunk[i] || '' ) + '</td>';
-
-          }
-
-          for ( var i = chunk.length, l = this.columns_nr; i < l; i++ ) {
-
-            row_html += '<td class="fillable"></td>';
-
-          }
-
-          row_html += '</tr>';
-
-          this.$tbody.append ( row_html );
+          $rows = $rows.add ( rowHtml );
 
         }
 
-        this._check_empty ();
+        this.$body.append ( $rows );
+
+        this._checkEmpty ();
 
         this.$table.trigger ( 'change' );
 
-        this._trigger ( 'add' );
+        this._trigger ( 'add', {
+          $rows: $rows
+        });
 
       }
 
@@ -8319,25 +8315,27 @@ $(function () {
     update: function ( id ) { //INFO: id, datas...
 
       var datas = _.tail ( arguments ),
-        $row = $( '.' + this._get_row_id ( id ) );
+          $row = $( '.' + this._getRowId ( id ) );
 
-      if ( datas.length && $row.length ) {
+      if ( datas.length > 0 && $row.length === 1 ) {
 
-        var $tds = $row.find ( 'td' );
+        var $rowCells = $row.find ( this.options.selectors.rowCell );
 
-        _.each ( datas, function ( data, index ) {
+        for ( var i = 0, l = datas.length; i < l; i++ ) {
 
-          if ( _.isString ( data ) ) {
+          if ( _.isString ( datas[i] ) ) {
 
-            $tds.eq ( index ).html ( data );
+            $rowCells.eq ( i ).html ( datas[i] );
 
           }
 
-        });
+        }
 
         this.$table.trigger ( 'change' );
 
-        this._trigger ( 'update' );
+        this._trigger ( 'update', {
+          $row: $row
+        });
 
       }
 
@@ -8347,17 +8345,19 @@ $(function () {
 
     remove: function ( id ) {
 
-      var $row = $( '.' + this._get_row_id ( id ) );
+      var $row = $( '.' + this._getRowId ( id ) );
 
-      if ( $row.length ) {
+      if ( $row.length === 1 ) {
 
         $row.remove ();
 
-        this._check_empty ();
+        this._checkEmpty ();
 
         this.$table.trigger ( 'change' );
 
-        this._trigger ( 'remove' );
+        this._trigger ( 'remove', {
+          $row: $row
+        });
 
       }
 
@@ -8367,17 +8367,19 @@ $(function () {
 
     clear: function () {
 
-      var $rows = this.$tbody.find ( 'tr:not(.table-row-empty)' );
+      var $rows = this.$body.find ( this.options.selectors.notEmptyRow );
 
-      if ( $rows.length ) {
+      if ( $rows.length > 0 ) {
 
         $rows.remove ();
 
-        this._check_empty ();
+        this._checkEmpty ();
 
         this.$table.trigger ( 'change' );
 
-        this._trigger ( 'clear' );
+        this._trigger ( 'clear', {
+          $rows: $rows
+        });
 
       }
 
@@ -8391,7 +8393,7 @@ $(function () {
 
   $(function () {
 
-    $('table').tableHelper ();
+    $('table.table').tableHelper ();
 
   });
 
