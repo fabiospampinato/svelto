8d221d376d8841a2ae25cad4072aabbfe8b515be
FabioSpampinato <spampinabio@gmail.com>
2015-11-24 16:06:06 +0100

    Gulp, added customizable destination folders, by default on development writing inside `svelto-website`

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,19 @@
 * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
 * ========================================================================= */
 
+/* DESTINATIONS */
+
+var DEST = {
+  css: 'dist/css',
+  cssMin : 'dist/css',
+  fonts: 'dist/fonts',
+  images: 'dist/images',
+  js: 'dist/js',
+  jsMin: 'dist/js',
+  scss: 'dist/scss',
+  scssParts: 'dist/scss'
+};
+
 /* REQUIRES */
 
 var _           = require ( 'lodash' ),
@@ -48,13 +61,23 @@ var autoprefixer = require ( 'gulp-autoprefixer' ),
 var isProduction  = !!argv.production,
     isDevelopment = !isProduction;
 
+/* DEVELOPMENT DESTS */
+
+if ( isDevelopment ) {
+
+  DEST.images = '../svelto-website/app/public';
+  DEST.js = '../svelto-website/app/svelto/client/lib';
+  DEST.scss = '../svelto-website/app/svelto/client/stylesheet';
+
+}
+
 /* FONTS */
 
 gulp.task ( 'fonts', function () {
 
   return gulp.src ( 'src/**/*.{eot,ttf,woff,woff2}' )
              .pipe ( flatten () )
-             .pipe ( gulp.dest ( 'dist/fonts' ) );
+             .pipe ( gulp.dest ( DEST.fonts ) );
 
 });
 
@@ -67,11 +90,11 @@ gulp.task ( 'images', function () {
 
   return gulp.src ( 'src/**/*.{bmp,gif,ico,jpg,jpeg,png,svg}' )
              .pipe ( newer ({
-               dest: 'dist/images',
+               dest: DEST.images,
                map: path.basename
              }))
              .pipe ( gulpif ( isProduction, bytediff.start () ) )
-             .pipe ( gulpif ( isProduction && !isProduction, imagemin ({ //TODO: Reenable, but fix the svg compression, right not it strips out pretty much everything, even the image
+             .pipe ( gulpif ( isProduction && !isProduction, imagemin ({ //TODO: Re-enable, but fix the svg compression, right not it strips out pretty much everything, even the image
                interlaced: true, //INFO: Affects GIF images
                progressive: true, //INFO: Affects JPG images
                optimizationLevel: 7, //INFO: Affects PNG images
@@ -83,7 +106,7 @@ gulp.task ( 'images', function () {
              })))
              .pipe ( gulpif ( isProduction, bytediff.stop () ) )
              .pipe ( flatten () )
-             .pipe ( gulp.dest ( 'dist/images' ) );
+             .pipe ( gulp.dest ( DEST.images ) );
 
 });
 
@@ -128,17 +151,17 @@ gulp.task ( 'js', ['js-temp'], function () {
   if ( isDevelopment ) {
 
     return gulp.src ( '.temp/js/*.js' )
-               .pipe ( newer ( 'dist/js/svelto.js' ) )
+               .pipe ( newer ( DEST.js + '/svelto.js' ) )
                .pipe ( sort () )
                .pipe ( concat ( 'svelto.js' ) )
-               .pipe ( gulp.dest ( 'dist/js' ) )
+               .pipe ( gulp.dest ( DEST.js ) )
                .pipe ( rename ( 'svelto.min.js' ) )
-               .pipe ( gulp.dest ( 'dist/js' ) );
+               .pipe ( gulp.dest ( DEST.jsMin ) );
 
   } else {
 
     return gulp.src ( 'src/**/*.js' )
-              //  .pipe ( newer ( 'dist/js/svelto.js' ) ) //FIXME: Maybe nothing is changed in the files, but we switched between development and production so we should recompile
+              //  .pipe ( newer ( DEST.js + '/svelto.js' ) ) //FIXME: Maybe nothing is changed in the files, but we switched between development and production so we should recompile
                .pipe ( sort () )
                .pipe ( dependencies ({
                  pattern: /\* @requires [\s-]*(.*\.js)/g
@@ -149,10 +172,10 @@ gulp.task ( 'js', ['js-temp'], function () {
                .on ( 'error', function ( err ) {
                  gutil.log ( err.message );
                })
-               .pipe ( gulp.dest ( 'dist/js' ) )
+               .pipe ( gulp.dest ( DEST.js ) )
                .pipe ( uglify () )
                .pipe ( rename ( 'svelto.min.js' ) )
-               .pipe ( gulp.dest ( 'dist/js' ) );
+               .pipe ( gulp.dest ( DEST.jsMin ) );
 
   }
 
@@ -165,8 +188,8 @@ gulp.task ( 'js', ['js-temp'], function () {
 
 gulp.task ( 'css', ['scss-full'], function () {
 
- return gulp.src ( 'dist/scss/svelto.scss' )
-            .pipe ( newer ( 'dist/css/svelto.css' ) )
+ return gulp.src ( DEST.scss + '/svelto.scss' )
+            .pipe ( newer ( DEST.css + '/svelto.css' ) )
             .pipe ( sass ({
               outputStyle: 'expanded',
               precision: 10
@@ -177,54 +200,54 @@ gulp.task ( 'css', ['scss-full'], function () {
               remove: true
             })))
             .pipe ( rename ( 'svelto.css' ) )
-            .pipe ( gulp.dest ( 'dist/css' ) )
+            .pipe ( gulp.dest ( DEST.css ) )
             .pipe ( gulpif ( isProduction, csso () ) )
             .pipe ( gulpif ( isProduction, minify_css ({
               keepSpecialComments: 0,
               roundingPrecision: -1
             })))
             .pipe ( rename ( 'svelto.min.css' ) )
-            .pipe ( gulp.dest ( 'dist/css' ) );
+            .pipe ( gulp.dest ( DEST.cssMin ) );
 
 });
 
 gulp.task ( 'scss-full', ['scss-parts'], function () {
 
- return gulp.src ( ['dist/scss/svelto.variables.scss', 'dist/scss/svelto.mixins.scss', 'dist/scss/svelto.components.scss'] )
-            .pipe ( newer ( 'dist/scss/svelto.scss' ) )
+ return gulp.src ( [ DEST.scssParts + '/svelto.variables.scss', DEST.scssParts + '/svelto.mixins.scss', DEST.scssParts + '/svelto.components.scss'] )
+            .pipe ( newer ( DEST.scss + '/svelto.scss' ) )
             .pipe ( concat ( 'svelto.scss' ) )
-            .pipe ( gulp.dest ( 'dist/scss' ) );
+            .pipe ( gulp.dest ( DEST.scss ) );
 
 });
 
 gulp.task ( 'scss-parts', function () {
 
   var stream_variables = gulp.src ( ['src/**/variables.scss', 'src/variables/**/*.scss'] )
-                             .pipe ( newer ( 'dist/scss/svelto.variables.scss' ) )
+                             .pipe ( newer ( DEST.scssParts + '/svelto.variables.scss' ) )
                              .pipe ( sort () )
                              .pipe ( dependencies ({
                                pattern: /\* @requires [\s-]*(.*\.scss)/g
                              }))
                              .pipe ( concat ( 'svelto.variables.scss' ) )
-                             .pipe ( gulp.dest ( 'dist/scss' ) );
+                             .pipe ( gulp.dest ( DEST.scssParts ) );
 
   var stream_mixins = gulp.src ( ['src/**/mixins.scss', 'src/mixins/**/*.scss'] )
-                          .pipe ( newer ( 'dist/scss/svelto.mixins.scss' ) )
+                          .pipe ( newer ( DEST.scssParts + '/svelto.mixins.scss' ) )
                           .pipe ( sort () )
                           .pipe ( dependencies ({
                             pattern: /\* @requires [\s-]*(.*\.scss)/g
                           }))
                           .pipe ( concat ( 'svelto.mixins.scss' ) )
-                          .pipe ( gulp.dest ( 'dist/scss' ) );
+                          .pipe ( gulp.dest ( DEST.scssParts ) );
 
   var stream_others = gulp.src ( ['src/**/*.scss', '!src/**/variables.scss', '!src/variables/**/*.scss', '!src/**/mixins.scss', '!src/mixins/**/*.scss'] )
-                          .pipe ( newer ( 'dist/scss/svelto.components.scss' ) )
+                          .pipe ( newer ( DEST.scssParts + '/svelto.components.scss' ) )
                           .pipe ( sort () )
                           .pipe ( dependencies ({
                             pattern: /\* @requires [\s-]*(.*\.scss)/g
                           }))
                           .pipe ( concat ( 'svelto.components.scss' ) )
-                          .pipe ( gulp.dest ( 'dist/scss' ) );
+                          .pipe ( gulp.dest ( DEST.scssParts ) );
 
   return merge ( stream_variables, stream_mixins, stream_others );
 
