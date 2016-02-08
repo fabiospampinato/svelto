2f16e63ea77996712ddf9b416892c00c43348508
FabioSpampinato <spampinabio@gmail.com>
2016-02-08 23:57:03 +0100

    Added browser-sync support, very useful for multi-browsers testing

--- tasks/demo.js
+++ tasks/demo.js
@@ -8,7 +8,9 @@
 
 /* REQUIRE */
 
-var path  = require ( 'path' ),
+var _     = require ( 'lodash' ),
+    argv  = require ( 'yargs' ).argv,
+    path  = require ( 'path' ),
     spawn = require ( 'child_process' ).spawn,
     gulp  = require ( 'gulp-help' )( require ( 'gulp' ) );
 
@@ -24,13 +26,28 @@ var logBuffer = function ( buffer ) {
 
 gulp.task ( 'demo', 'Serve the demos', function () {
 
-  var meteor = spawn ( 'meteor', ['run', '--port', '3030'], { cwd: path.join ( process.cwd (), '/demo' ) } );
+  var demoPath = path.join ( process.cwd (), '/demo' ),
+      port = !_.isNaN ( Number ( argv.port ) ) ? Number ( argv.port ) : 3333,
+      bsport = !_.isNaN ( Number ( argv.bsport ) ) ? Number ( argv.bsport ) : 4444,
+      uiport = !_.isNaN ( Number ( argv.uiport ) ) ? Number ( argv.uiport ) : 5555;
+
+  var meteor = spawn ( 'meteor', ['run', '--port', port], { cwd: demoPath } );
 
   meteor.stdout.on ( 'data', logBuffer );
   meteor.stderr.on ( 'data', logBuffer );
 
+  var bs = spawn ( 'browser-sync', ['start', '--port', bsport, '--ui-port', uiport, '--proxy', 'localhost:' + port, '--no-open', '--files', '**/*.html, **/*.js, **/*.css'], { cwd: demoPath } );
+
+  bs.stdout.on ( 'data', logBuffer );
+  bs.stderr.on ( 'data', logBuffer );
+
 }, {
 
-  aliases: ['serve-demo']
+  aliases: ['serve-demo'],
+  options: {
+    'port=number': 'Demos will be available at localhost:port [default=3333]',
+    'bsport=number': 'Browser-sync-enabled demos will be available at localhost:bsport [default=4444]',
+    'uiport=number': 'Browser-sync\'s UI port [default=5555]'
+  }
 
 });
