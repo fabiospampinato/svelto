0f3c4c0ad834d38f086ed7baefe62b44d03a37d5
FabioSpampinato <spampinabio@gmail.com>
2016-02-11 17:21:05 +0100

    Reorganized `blurred`

--- src/helpers/blurred/blurred.js
+++ src/helpers/blurred/blurred.js
@@ -5,10 +5,8 @@
  * Copyright (c) 2015-2016 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @require ../core/core.js
+ * @require ../../core/core.js
  * ========================================================================= */
-
-//TODO: Maybe rename it
 
 (function ( $, _, Svelto ) {
 
