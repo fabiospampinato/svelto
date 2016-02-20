100ca0200fa2d3ee0416f4c40907a101525c2fcd
FabioSpampinato <spampinabio@gmail.com>
2016-02-20 21:03:01 +0100

    Rewritten `filter` plugin

--- tasks/plugins/filter.js
+++ tasks/plugins/filter.js
@@ -8,45 +8,172 @@
 
 /* REQUIRE */
 
-var _            = require ( 'lodash' ),
-    path         = require ( 'path' ),
-    streamfilter = require ( 'streamfilter' ),
-    project      = require ( '../config/project' ),
-    components   = []; //project.components.map ( function ( component ) { return component.split ( '/' ); } );
+var _       = require ( 'lodash' ),
+    path    = require ( 'path' ),
+    through = require ( 'through2' ),
+    project = require ( '../config/project' ),
+    gutil   = require ( 'gulp-util' );
 
-/* FILTER */
+/* COMPONENTS */
 
-var filter = function () {
+var parseComponents = function ( obj, prefix ) {
 
-  return streamfilter ( function ( file, encoding, callback ) {
+  var parsed = {};
 
-    var relative = file.relative;
+  for ( var key in obj ) {
 
-    if ( relative.indexOf ( path.sep ) === -1 ) return callback ( false );
+    if ( obj.hasOwnProperty ( key ) ) {
 
-    var parts = path.dirname ( relative ).split ( path.sep ),
-        pl = parts.length;
+      var value = obj[key];
 
-    for ( var i = 0, l = components.length; i < l; i++ ) {
+      if ( _.isBoolean ( value ) ) {
 
-      var component = components[i],
-          cl = component.length;
+        parsed[_.trim ( prefix + key, '/' )] = value;
 
-      if ( pl > cl && _.isEqual ( parts.slice ( 0, cl ), component ) ||
-           pl < cl && _.isEqual ( parts, component.slice ( 0, pl ) ) ||
-           pl === cl && _.isEqual ( parts, component ) ) {
+      } else if ( _.isPlainObject ( value ) ) {
 
-        return callback ( false );
+        parsed[_.trim ( prefix + key, '/' )] = true;
+
+        _.extend ( parsed, parseComponents ( value, prefix + key + '/' ) );
 
       }
 
     }
 
-    callback ( true );
+  }
 
-  }, {
+  return parsed;
 
-    objectMode: true
+};
+
+/* UTILITIES */
+
+var needsFiltering = function ( components, file ) {
+
+  var relative = path.dirname ( file.relative ).replace ( /\\\\/g, '/' ).replace ( /\/\//g, '/' ),
+      maxPriority = relative.split ( '/' ).length,
+      priority = 0,
+      needs = false;
+
+  if ( relative.indexOf ( path.sep ) === - 1 ) { //TODO: Remove it
+    throw new Error ( 'needed check, apparently... at lest for: ' + relative );
+  }
+
+  for ( var component in components ) {
+
+    if ( components.hasOwnProperty ( component ) ) {
+
+      var newPriority = component.split ( '/' ).length;
+
+      if ( newPriority > priority && newPriority <= maxPriority && _.startsWith ( relative, component ) ) {
+
+        priority = newPriority;
+        needs = !components[component];
+
+      }
+
+    }
+
+  }
+
+  return needs;
+
+};
+
+var partitionFiles = function ( files, components ) {
+
+  return _.partition ( files, _.partial ( needsFiltering, components ) );
+
+};
+
+var logFiltered = function ( files ) {
+
+  if ( files.length ) {
+
+    var list = 'Filtered files:\n';
+
+    for ( var i = 0, l = files.length; i < l; i++ ) {
+
+      list += _.padRight ( i + 1, l.toString ().length ) + ' - ' + files[i].path;
+
+      if ( i + 1 < l ) {
+
+        list += '\n';
+
+      }
+
+    }
+
+    console.log ( list );
+
+  } else {
+
+    console.log ( 'No files have been filtered' );
+
+  }
+
+};
+
+/* WORKER */
+
+var worker = function ( files, config, components ) {
+
+  var partition = partitionFiles ( files, components ),
+      filtered = partition[0],
+      allowed = partition[1];
+
+  if ( config.log ) {
+
+    logFiltered ( filtered );
+
+  }
+
+  return allowed;
+
+};
+
+/* FILTER */
+
+var filter = function ( config ) {
+
+  /* CONFIG */
+
+  config = _.merge ({
+    log: false
+  }, config );
+
+  /* VARIABLES */
+
+  var files = [],
+      components = parseComponents ( project.components, '' );
+
+  /* EXTEND */
+
+  return through.obj ( function ( file, encoding, callback ) {
+
+    files.push ( file );
+
+    callback ();
+
+  }, function ( callback ) {
+
+    files = worker ( files, config, components );
+
+    if ( files instanceof gutil.PluginError ) {
+
+      callback ( files );
+
+    } else {
+
+      for ( var i = 0, l = files.length; i < l; i++ ) {
+
+        this.push ( files[i] );
+
+      }
+
+      callback ();
+
+    }
 
   });
 
