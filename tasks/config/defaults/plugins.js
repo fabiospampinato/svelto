148560807d101d6def09272b042988e4688859bf
FabioSpampinato <spampinabio@gmail.com>
2016-02-22 11:48:45 +0100

    Added `Edge` to autoprefixer

--- tasks/config/defaults/plugins.js
+++ tasks/config/defaults/plugins.js
@@ -12,7 +12,7 @@ var plugins = {
   autoprefixer: {
     enabled: true,
     options: {
-      browsers: ['ie >= 10', 'ie_mob >= 10', 'ff >= 30', 'chrome >= 34', 'safari >= 7', 'opera >= 23', 'ios >= 7', 'android >= 4.4', 'bb >= 10'],
+      browsers: ['ie >= 10', 'ie_mob >= 10', 'edge >= 13', 'ff >= 30', 'chrome >= 34', 'safari >= 7', 'opera >= 23', 'ios >= 7', 'android >= 4.4', 'bb >= 10'],
       cascade: true,
     }
   },
