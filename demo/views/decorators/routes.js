7cfc48fcb250cd36d6253462218dc17e65a5b7d4
FabioSpampinato <spampinabio@gmail.com>
2016-03-07 02:16:22 +0100

    Updated demo

--- demo/views/decorators/routes.js
+++ demo/views/decorators/routes.js
@@ -1,6 +1,6 @@
 
 /* DECORATORS */
 
-let templates = ['actionable', 'animations', 'attached', 'blurred', 'border_radius', 'centered', 'colors', 'disabled', 'grid', 'hidden', 'highlighted', 'limited', 'order', 'outlined', 'raisable', 'scrollable', 'sizes', 'z_depths'];
+let templates = ['actionable', 'animations', 'attached', 'blurred', 'border_radius', 'centered', 'colors', 'disabled', 'grid', 'hidden', 'highlighted', 'limited', 'obscured', 'order', 'outlined', 'raisable', 'scrollable', 'sizes', 'z_depths'];
 
 SWRouter.pages ( templates, '/decorators/' );
--- demo/views/widgets/routes.js
+++ demo/views/widgets/routes.js
@@ -1,27 +1,6 @@
 
-/* COMPONENTS */
+/* WIDGETS */
 
-let components = ['accordion', 'badge', 'breadcrumb', 'button', 'card', 'carousel', 'centerer', 'chat', 'checkbox', 'colorpicker', 'container', 'datepicker', 'divider', 'draggable', 'dropdown', 'droppable', 'embedded', 'expander', 'figure', 'flag', 'flickable', 'flippable', 'footer', 'form', 'form_ajax', 'form_sync', 'form_validate', 'fullscreen', 'header', 'icon', 'icons', 'image', 'infobar', 'input', 'label', 'modal', 'noty', 'overlay', 'pager', 'pagination', 'panel', 'parallax', 'placeholder', 'progressbar', 'radio', 'rater', 'remote_action', 'remote_modal', 'ripple', 'scroll', 'sections', 'select', 'selectable', 'shape', 'slider', 'sortable', 'spacer', 'spinner', 'spinner_overlay', 'statistic', 'stepper', 'switch', 'table', 'tabs', 'tagbox', 'textarea', 'time_ago', 'toolbar', 'tooltip', 'typography'];
+let templates = ['accordion', 'badge', 'breadcrumb', 'button', 'card', 'carousel', 'centerer', 'chat', 'checkbox', 'colorpicker', 'container', 'datepicker', 'divider', 'draggable', 'dropdown', 'droppable', 'embedded', 'expander', 'figure', 'flag', 'flickable', 'flippable', 'footer', 'form', 'form_ajax', 'form_sync', 'form_validate', 'fullscreen', 'header', 'icon', 'icons', 'image', 'infobar', 'input', 'input_autogrow', 'label', 'modal', 'noty', 'overlay', 'pager', 'pagination', 'panel', 'parallax', 'placeholder', 'progressbar', 'radio', 'rater', 'remote_action', 'remote_modal', 'ripple', 'scroll', 'sections', 'select', 'selectable', 'shape', 'slider', 'sortable', 'spacer', 'spinner', 'spinner_overlay', 'statistic', 'stepper', 'switch', 'table', 'tabs', 'tagbox', 'textarea', 'textarea_autogrow', 'time_ago', 'toolbar', 'tooltip', 'typography'];
 
-for ( let component of components ) {
-
-  Router.route ( '/widgets/' + component, {
-    layoutTemplate: 'master',
-    yieldRegions: {
-      [component]: { to: 'content' }
-    }
-  });
-
-  /* WIDGETIZE */
-
-  if ( Meteor.isClient ) {
-
-    Template[component].onRendered ( function () {
-
-      $(document.body).widgetize ();
-
-    });
-
-  }
-
-}
+SWRouter.pages ( templates, '/widgets/' );
