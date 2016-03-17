bfb7db07db901b8d98ea442fa4fe392f2a9652b5
FabioSpampinato <spampinabio@gmail.com>
2016-03-17 22:31:58 +0100

    Added `$.fn.isAttached`

--- src/core/jquery/helpers/is_attached.js
+++ src/core/jquery/helpers/is_attached.js
@@ -1,19 +1,23 @@
 
 /* =========================================================================
- * Svelto - Core - jQuery - Helpers
+ * Svelto - Core - jQuery - Helpers (Is attached)
  * =========================================================================
  * Copyright (c) 2015-2016 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @require ./clean_data.js
- * @require ./event_xy.js
- * @require ./frame.js
- * @require ./get_rect.js
- * @require ./hsl.js
- * @require ./is_attached.js
- * @require ./iterator.js
- * @require ./scroll.js
- * @require ./selection.js
- * @require ./top_index.js
- * @require ./z_index.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( $ ) {
+
+  'use strict';
+
+  /* IS ATTACHED */
+
+  $.fn.isAttached = function () {
+
+    return $.contains ( document.documentElement, this[0] );
+
+  };
+
+}( jQuery ));
