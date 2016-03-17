8b0a35c2457b92abd399b7fb1d3c7c21e0940337
FabioSpampinato <spampinabio@gmail.com>
2016-03-17 15:26:59 +0100

    Updated `input[type="file"]` added `.input-file-names`

---
+++ src/widgets/input/file/names/names.js
@@ -0,0 +1,111 @@
+
+/* =========================================================================
+ * Svelto - Widgets - Input - File - Names
+ * =========================================================================
+ * Copyright (c) 2015-2016 Fabio Spampinato
+ * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
+ * =========================================================================
+ * @require core/widget/widget.js
+ * ========================================================================= */
+
+(function ( $, _, Svelto, Widgets, Factory ) {
+
+  'use strict';
+
+  /* CONFIG */
+
+  let config = {
+    name: 'inputFileNames',
+    plugin: true,
+    selector: '.input-file-names',
+    options: {
+      placeholder: 'Select a file...',
+      callbacks: {
+        change: _.noop
+      }
+    }
+  };
+
+  /* INPUT FILE NAMES */
+
+  class InputFileNames extends Widgets.Widget {
+
+    /* SPECIAL */
+
+    _variables () {
+
+      this.$names = this.$element;
+
+      this.$input = this.$names.closest ( 'label' ).find ( 'input[type="file"]' );
+      this.input = this.$input[0];
+
+    }
+
+    _init () {
+
+      this.options.placeholder = this.$names.text () || this.options.placeholder;
+
+      this._update ();
+
+    }
+
+    _events () {
+
+      this.___change ();
+
+    }
+
+    /* PRIVATE */
+
+    _getNames () {
+
+      let names = [];
+
+      for ( let i = 0, l = this.input.files.length; i < l; i++ ) {
+
+        names.push ( this.input.files[i].name );
+
+      }
+
+      return names;
+
+    }
+
+    _getText () {
+
+      let names = this._getNames ();
+
+      return names.length ? names.join ( ', ' ) : this.options.placeholder;
+
+    }
+
+    /* CHANGE */
+
+    ___change () {
+
+      this._on ( true, this.$input, 'change', this._update );
+
+    }
+
+    /* UPDATE */
+
+    _update () {
+
+      let previous = this.$names.text (),
+          current = this._getText ();
+
+      if ( previous === current ) return;
+
+      this.$names.text ( current );
+
+      this._trigger ( 'change' );
+
+    }
+
+  }
+
+  /* FACTORY */
+
+  Factory.init ( InputFileNames, config, Widgets );
+
+}( Svelto.$, Svelto._, Svelto, Svelto.Widgets, Svelto.Factory ));
