95b3ea6686686323f5049800627cfca543431a96
FabioSpampinato <spampinabio@gmail.com>
2016-03-17 18:32:43 +0100

    Updated demo

--- demo/views/form-ajax/routes.js
+++ demo/views/form-ajax/routes.js
@@ -1,7 +1,7 @@
 
-/* FORM AJAX */
+/* FORM AJAX BASIC */
 
-Router.route ( '/form-ajax-test', function () {
+Router.route ( '/form-ajax-basic', function () {
 
   let form = new formidable.IncomingForm ();
 
@@ -11,6 +11,26 @@ Router.route ( '/form-ajax-test', function () {
 
       this.response.end ( JSON.stringify ({
         msg: 'Form submitted using ajax! What\'s "' + fields.input_1 + '"?'
+      }));
+
+    }, 1500 );
+
+  });
+
+}, { where: 'server' });
+
+/* FORM AJAX FILE */
+
+Router.route ( '/form-ajax-file', function () {
+
+  let form = new formidable.IncomingForm ();
+
+  form.parse ( this.request, ( err, fields, files ) => {
+
+    setTimeout ( () => {
+
+      this.response.end ( JSON.stringify ({
+        msg: 'Form submitted using ajax! "' + files.file.name + '" has been uploaded too!'
       }));
 
     }, 1500 );
