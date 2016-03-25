a9a16d419314b7db5ada6b7fe242451998571b86
FabioSpampinato <spampinabio@gmail.com>
2016-03-25 03:08:23 +0100

    Added a `Scrollbar size` Modernizr test

--- src/core/modernizr/tests/scrollbar_size.js
+++ src/core/modernizr/tests/scrollbar_size.js
@@ -1,16 +1,23 @@
 
 /* =========================================================================
- * Svelto - Core - Modernizr - Tests
+ * Svelto - Core - Modernizr - Tests (Scrollbar size)
  * =========================================================================
  * Copyright (c) 2015-2016 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @require ./clip_path_polygon.js
- * @require ./clip_path_url.js
- * @require ./flexbox.js
- * @require ./flexbox_legacy.js
- * @require ./flexbox_tweener.js
- * @require ./overlay_scrollbars.js
- * @require ./position_sticky.js
- * @require ./scrollbar_size.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( Modernizr ) {
+
+  'use strict';
+
+  /* SCROLLBAR SIZE */
+
+  let size;
+
+  Modernizr.testStyles ( '#modernizr {width:100px;height:100px;overflow:scroll}', ele => size = ele.offsetWidth - ele.clientWidth );
+
+  Modernizr.addTest ( 'scrollbar-size-' + size, true );
+
+}( Modernizr ));
