75f9883b31f53bb01ae1fe5fd7c2319e8422c060
FabioSpampinato <spampinabio@gmail.com>
2016-03-27 16:18:09 +0200

    Updated package-specific settings

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe ({
   name: 'svelto:svelto',
-  summary: 'Modular front end framework for modern browsers, with battery included: 50+ components & 20+ tools.',
+  summary: 'Modular front end framework for modern browsers, with battery included: 100+ widgets and tools.',
   version: '0.5.0',
   git: 'https://github.com/svelto/svelto.git'
 });
@@ -11,20 +11,16 @@ Package.onUse ( function ( api ) {
   api.versionsFrom ( 'METEOR@1.0' );
 
   api.use ( 'ecmascript@0.1.6', 'client' );
-  api.use ( 'stevezhu:lodash@3.10.1', 'client' );
+  api.use ( 'stevezhu:lodash@4.6.1', 'client' );
   api.use ( 'jquery@1.11.4', 'client' );
 
   //TODO: Add auto assets array generation
 
   var assets = [
-    'dist/fonts/MaterialIcons-Regular.eot',
-    'dist/fonts/MaterialIcons-Regular.ttf',
     'dist/fonts/MaterialIcons-Regular.woff',
     'dist/fonts/MaterialIcons-Regular.woff2',
-    'dist/images/blurred.svg',
-    'dist/images/chat-clips.svg',
-    'dist/images/dropdown-clips.svg',
-    'dist/images/flags.png'
+    'dist/images/flags.png',
+    'dist/images/pointing-clips.svg'
   ];
 
   if ( api.addAssets ) {
