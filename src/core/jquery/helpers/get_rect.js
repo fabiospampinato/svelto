81287346714d439b6ac10a19cca23b5efed0ef65
FabioSpampinato <spampinabio@gmail.com>
2016-04-29 00:01:48 +0200

    Added `window` support to `$.getRect` via `$.getWindowRect`

--- src/core/jquery/helpers/get_rect.js
+++ src/core/jquery/helpers/get_rect.js
@@ -16,13 +16,30 @@
 
   $.getRect = function ( node ) {
 
-    return node.getBoundingClientRect ();
+    return node === window ? $.getWindowRect () : node.getBoundingClientRect ();
 
   };
 
   $.fn.getRect = function () {
 
-    return this.length ? this[0].getBoundingClientRect () : undefined;
+    return this.length ? $.getRect ( this[0] ) : undefined;
+
+  };
+
+  /* WINDOW RECT */
+
+  $.getWindowRect = function () {
+
+    let rect = {};
+
+    rect.left = 0;
+    rect.top = 0;
+    rect.width = window.innerWidth;
+    rect.height = window.innerHeight;
+    rect.right = rect.width;
+    rect.bottom = rect.height;
+
+    return rect;
 
   };
 
