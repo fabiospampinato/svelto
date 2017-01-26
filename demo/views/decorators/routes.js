3115688ea573362e9950349a7fb2a5eaf830043d
Fabio Spampinato <spampinabio@gmail.com>
2017-01-26 15:35:55 +0100

    Added a `target` decorator

--- demo/views/decorators/routes.js
+++ demo/views/decorators/routes.js
@@ -1,6 +1,6 @@
 
 /* DECORATORS */
 
-let templates = ['actionable', 'animations', 'attached', 'blurred', 'border_radius', 'bordered', 'centered', 'colors', 'disabled', 'floated', 'fluid', 'grid', 'hidden', 'highlighted', 'limited', 'obscured', 'order', 'pointing', 'raisable', 'scrollable', 'sizes', 'spaced', 'z_depths'];
+let templates = ['actionable', 'animations', 'attached', 'blurred', 'border_radius', 'bordered', 'centered', 'colors', 'disabled', 'floated', 'fluid', 'grid', 'hidden', 'highlighted', 'limited', 'obscured', 'order', 'pointing', 'raisable', 'scrollable', 'sizes', 'spaced', 'target', 'z_depths'];
 
 SRouter.pages ( templates, '/decorators/' );
