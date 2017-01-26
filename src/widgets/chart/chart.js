237dd0da04794ee4c834cccb763fa19d745b5b2e
Fabio Spampinato <spampinabio@gmail.com>
2017-01-26 12:03:17 +0100

    Added a chart widget

---
+++ src/widgets/chart/chart.js
@@ -0,0 +1,99 @@
+
+/* =========================================================================
+ * Svelto - Widgets - Chart
+ * =========================================================================
+ * Copyright (c) 2015-2017 Fabio Spampinato
+ * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
+ * =========================================================================
+ * @require ./config.js
+ * @require core/widget/widget.js
+ * ========================================================================= */
+
+/* CHART */
+
+(function ( $, _, Svelto, Widgets, Factory, ChartLib ) {
+
+  'use strict';
+
+  /* CHECK IF LOADED */
+
+  if ( !ChartLib ) return;
+
+  /* CONFIG */
+
+  let config = {
+    name: 'chart',
+    plugin: true,
+    selector: 'canvas.chart',
+    options: {
+      defaults: {
+        type: 'line',
+        colors: ['#1565c0'], // Primary color
+        labels: ['Dataset'],
+        datas: [[]]
+      },
+      datas: {
+        type: 'type',
+        colors: 'colors',
+        labels: 'labels',
+        datas: 'datas'
+      }
+    }
+  };
+
+  /* CHART */
+
+  class Chart extends Widgets.Widget {
+
+    /* SPECIAL */
+
+    _variables () {
+
+      this.$chart = this.$element;
+      this.chart = this.element;
+
+      let properties = ['type', 'colors', 'labels', 'datas'];
+
+      for ( let property of properties ) {
+
+        this[property] = this.$chart.data ( this.options.datas[property] ) || this.options.defaults[property];
+
+      }
+
+      if ( !_.isArray ( this.datas[0] ) ) this.datas = [this.datas];
+
+    }
+
+    _init () {
+
+      let settings = this._getSettings ();
+
+      this.instance = new ChartLib ( this.chart, settings );
+
+    }
+
+    /* PRIVATE */
+
+    _getSettings () {
+
+      return {
+        type: this.type,
+        data: {
+          labels: this.datas[0].map ( ( point, index ) => index + 1 ),
+          datasets: _.map ( this.labels, ( label, index ) => ({
+            label,
+            data: this.datas[index],
+            backgroundColor: this.colors[index]
+          }))
+        }
+      };
+
+    }
+
+  }
+
+  /* FACTORY */
+
+  Factory.init ( Chart, config, Widgets );
+
+}( Svelto.$, Svelto._, Svelto, Svelto.Widgets, Svelto.Factory, window.Chart ));
---
+++ src/widgets/chart/config.js
@@ -0,0 +1,52 @@
+
+/* =========================================================================
+ * Svelto - Widgets - Chart (Config)
+ * =========================================================================
+ * Copyright (c) 2015-2017 Fabio Spampinato
+ * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
+ * =========================================================================
+ * @before ./vendor/Chart.js
+ * @require core/animations/animations.js
+ * ========================================================================= */
+
+/* CHART */
+
+(function ( $, _, Svelto, Animations, ChartLib ) {
+
+  'use strict';
+
+  /* CHECK IF LOADED */
+
+  if ( !ChartLib ) return;
+
+  /* DEFAULTS */
+
+  _.merge ( ChartLib.defaults.global, {
+    maintainAspectRatio: false,
+    hover: {
+      animationDuration: Animations.normal
+    },
+    defaultColor: '#ffffff', // White background color
+    defaultFontColor: '#3f3f3f', // White text color
+    defaultFontFamily: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', Arial, sans-serif", // Default font family
+    defaultFontSize: 13.4544,
+    legend: {
+      position: 'bottom',
+      labels: {
+        boxWidth: 16,
+        fontSize: 16,
+        padding: 10
+      }
+    },
+    tooltips: {
+      backgroundColor: '#212121', // Black background color
+      titleFontSize: 16,
+      bodyFontSize: 16,
+      footerFontSize: 16
+    },
+    animations: {
+      duration: Animations.normal
+    }
+  });
+
+}( Svelto.$, Svelto._, Svelto, Svelto.Animations, window.Chart ));
