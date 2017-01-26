ffe061dda4f3f5ac8d679a260f48438cafd9e459
Fabio Spampinato <spampinabio@gmail.com>
2017-01-26 13:02:44 +0100

    Added some `cursor` helper classes

--- demo/views/_/routes.js
+++ demo/views/_/routes.js
@@ -1,6 +1,6 @@
 
 /* _ */
 
-let templates = ['alignment', 'border', 'clearfix', 'display', 'flexbox', 'flip', 'float', 'margin', 'overflow', 'padding', 'position', 'rotation', 'screen_reader', 'text', 'visibility'];
+let templates = ['alignment', 'border', 'clearfix', 'cursor', 'display', 'flexbox', 'flip', 'float', 'margin', 'overflow', 'padding', 'position', 'rotation', 'screen_reader', 'text', 'visibility'];
 
 SRouter.pages ( templates, '/_/' );
