b2a396fd38af6f714d7e6653953c42210be69582
Fabio Spampinato <spampinabio@gmail.com>
2017-01-29 16:10:14 +0100

    Added a `DatatablesPager` component

---
+++ src/widgets/datatables/pager/pager.js
@@ -0,0 +1,83 @@
+
+/* =========================================================================
+ * Svelto - Widgets - Datatables - Pager
+ * =========================================================================
+ * Copyright (c) 2015-2017 Fabio Spampinato
+ * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
+ * =========================================================================
+ * @require ../datatables.js
+ * @require widgets/targeter/targeter.js
+ * ========================================================================= */
+
+//FIXME: Doesn't work with custom sorting
+
+(function ( $, _, Svelto, Widgets, Factory, Pointer ) {
+
+  'use strict';
+
+  /* CONFIG */
+
+  let config = {
+    name: 'datatablesPager',
+    plugin: true,
+    selector: '.datatables-pager',
+    options: {
+      widget: Widgets.Dt,
+      page: NaN, // The page to go to (higher priority than row number)
+      row: NaN, // The row number to go to
+      datas: {
+        page: 'page',
+        row: 'row'
+      }
+    }
+  };
+
+  /* DATATABLES PAGER */
+
+  class DatatablesPager extends Widgets.Targeter {
+
+    /* SPECIAL */
+
+    _init () {
+
+      this.options.page = Number ( this.$element.data ( this.options.datas.page ) ) || this.options.page;
+      this.options.row = Number ( this.$element.data ( this.options.datas.row ) ) || this.options.row;
+
+    }
+
+    _events () {
+
+      this.___targetRemove ();
+      this.___tap ();
+
+    }
+
+    /* TAP */
+
+    ___tap () {
+
+      this._on ( Pointer.tap, this.__tap );
+
+    }
+
+    __tap () {
+
+      this.go ();
+
+    }
+
+    /* API */
+
+    go ( page = this.options.page, row = this.options.row ) {
+
+      this._targetInstance.page ( page, row );
+
+    }
+
+  }
+
+  /* FACTORY */
+
+  Factory.init ( DatatablesPager, config, Widgets );
+
+}( Svelto.$, Svelto._, Svelto, Svelto.Widgets, Svelto.Factory, Svelto.Pointer ));
