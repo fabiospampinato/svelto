b863a23f38a45ca6a39b1c3c71183b97b79cd7a4
Fabio Spampinato <spampinabio@gmail.com>
2017-01-29 23:35:38 +0100

    Added a `TextareaSender` component
    It basically adds the ability to send the form when any of its textareas have the focus by pressing `cmd + enter` on mac and `ctrl + enter` elsewhere.

---
+++ src/widgets/textarea/sender/sender.js
@@ -0,0 +1,66 @@
+
+/* =========================================================================
+ * Svelto - Widgets - Textarea - Sender
+ * =========================================================================
+ * Copyright (c) 2015-2017 Fabio Spampinato
+ * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
+ * =========================================================================
+ * @require core/widget/widget.js
+ * ========================================================================= */
+
+(function ( $, _, Svelto, Widgets, Factory ) {
+
+  'use strict';
+
+  /* CONFIG */
+
+  let config = {
+    name: 'textareaSender',
+    plugin: true,
+    selector: 'form textarea',
+    options: {
+      selectors: {
+        form: 'form'
+      },
+      keystrokes: {
+        'ctmd + enter': 'send'
+      }
+    }
+  };
+
+  /* TEXTAREA SENDER */
+
+  class TextareaSender extends Widgets.Widget {
+
+    /* SPECIAL */
+
+    _variables () {
+
+      this.$textarea = this.$element;
+      this.$form = this.$textarea.closest ( this.options.selectors.form );
+
+    }
+
+    _events () {
+
+      this.___keydown ();
+
+    }
+
+    /* SEND */
+
+    send () {
+
+      if ( !$(document.activeElement).is ( this.$textarea ) ) return;
+
+      this.$form.submit ();
+
+    }
+
+  }
+
+  /* FACTORY */
+
+  Factory.init ( TextareaSender, config, Widgets );
+
+}( Svelto.$, Svelto._, Svelto, Svelto.Widgets, Svelto.Factory ));
