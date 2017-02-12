47c4585f6f25df1a5066fc83f48013e2df174f69
Fabio Spampinato <spampinabio@gmail.com>
2017-02-13 00:26:38 +0100

    jQuery: added a `uniqueAjax` helper

--- src/core/jquery/helpers/unique_ajax.js
+++ src/core/jquery/helpers/unique_ajax.js
@@ -1,23 +1,27 @@
 
 /* =========================================================================
- * Svelto - Core - jQuery - Helpers
+ * Svelto - Core - jQuery - Helpers (Unique AJAX)
  * =========================================================================
  * Copyright (c) 2015-2017 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @require ./clean_data.js
- * @require ./diff.js
- * @require ./event_namespacer.js
- * @require ./event_xy.js
- * @require ./frame.js
- * @require ./get_rect.js
- * @require ./hsl.js
- * @require ./is_attached.js
- * @require ./is_focused.js
- * @require ./iterator.js
- * @require ./scroll.js
- * @require ./selection.js
- * @require ./top_index.js
- * @require ./unique_ajax.js
- * @require ./z_index.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( $ ) {
+
+  'use strict';
+
+  /* UNIQUE AJAX */
+
+  $.uniqueAjax = function ( id, ...args ) {
+
+    if ( id in $.uniqueAjax.requests ) return $.uniqueAjax.requests[id];
+
+    return $.uniqueAjax.requests[id] = $.ajax ( ...args );
+
+  };
+
+  $.uniqueAjax.requests = {};
+
+}( jQuery ));
