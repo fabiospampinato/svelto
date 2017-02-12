284db19633d10b02e74da36c1618294ea563a968
Fabio Spampinato <spampinabio@gmail.com>
2017-02-13 00:26:20 +0100

    Lodash: added a `findMatches` helper

--- src/core/lodash/helpers/find_matches.js
+++ src/core/lodash/helpers/find_matches.js
@@ -1,16 +1,36 @@
 
 /* =========================================================================
- * Svelto - Core - lodash - Helpers
+ * Svelto - Core - lodash - Helpers (Find matches)
  * =========================================================================
  * Copyright (c) 2015-2017 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @require ./constants.js
- * @require ./find_matches.js
- * @require ./format.js
- * @require ./mkize.js
- * @require ./move.js
- * @require ./now_secs.js
- * @require ./round_closer.js
- * @require ./time_ago.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( _ ) {
+
+  'use strict';
+
+  /* FIND MATCHES */
+
+  _.mixin ({
+
+    findMatches ( str, regex ) {
+
+      let matches = [],
+          match;
+
+      while ( match = regex.exec ( str ) ) {
+
+        matches.push ( match );
+
+      }
+
+      return matches;
+
+    }
+
+  });
+
+}( lodash ));
