c6bc8cd4d32ca745c1e28306575d7f9b50d4ff7b
Fabio Spampinato <spampinabio@gmail.com>
2017-02-13 15:47:49 +0100

    Lodash: added a `replaceAll` helper

--- src/core/lodash/helpers/replace_all.js
+++ src/core/lodash/helpers/replace_all.js
@@ -1,17 +1,29 @@
 
 /* =========================================================================
- * Svelto - Core - lodash - Helpers
+ * Svelto - Core - lodash - Helpers (Replace all)
  * =========================================================================
  * Copyright (c) 2015-2017 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @require ./constants.js
- * @require ./find_matches.js
- * @require ./format.js
- * @require ./mkize.js
- * @require ./move.js
- * @require ./now_secs.js
- * @require ./replace_all.js
- * @require ./round_closer.js
- * @require ./time_ago.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( _ ) {
+
+  'use strict';
+
+  /* REPLACE ALL */
+
+  _.mixin ({
+
+    replaceAll ( string, pattern, replacement ) {
+
+      let escaped = pattern.replace ( /[.*+?^${}()|[\]\\]/g, '\\$&' );
+
+      return string.replace ( new RegExp ( escaped, 'g' ), replacement );
+
+    }
+
+  });
+
+}( lodash ));
