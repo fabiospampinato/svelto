d0e8425a071c46b1d92a7b67b84188a24644ff5b
Fabio Spampinato <spampinabio@gmail.com>
2017-10-28 20:45:57 +0200

    Added a `_.wait` helper

--- src/core/lodash/helpers/wait.js
+++ src/core/lodash/helpers/wait.js
@@ -1,19 +1,27 @@
 
 /* =========================================================================
- * Svelto - Core - lodash - Helpers
+ * Svelto - Core - lodash - Helpers (Wait)
  * =========================================================================
  * Copyright (c) 2015-2017 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @before ./constants.js
- * @before ./find_matches.js
- * @before ./format.js
- * @before ./frames.js
- * @before ./mkize.js
- * @before ./move.js
- * @before ./now_secs.js
- * @before ./replace_all.js
- * @before ./round_closer.js
- * @before ./time_ago.js
- * @before ./wait.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( _ ) {
+
+  'use strict';
+
+  /* WAIT */
+
+  _.mixin ({
+
+    wait ( ms ) {
+
+      return new Promise ( resolve => setTimeout ( resolve, ms ) );
+
+    }
+
+  });
+
+}( window.__svelto_lodash ));
