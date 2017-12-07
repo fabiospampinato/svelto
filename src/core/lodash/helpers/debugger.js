c3e0f049e90d08be0ca71498b6e92c86aec92bdb
Fabio Spampinato <spampinabio@gmail.com>
2017-12-07 15:23:15 +0100

    Lodash: added a `_.debugger` helper

--- src/core/lodash/helpers/debugger.js
+++ src/core/lodash/helpers/debugger.js
@@ -1,20 +1,27 @@
 
 /* =========================================================================
- * Svelto - Core - lodash - Helpers
+ * Svelto - Core - lodash - Helpers (Debugger)
  * =========================================================================
  * Copyright (c) 2015-2017 Fabio Spampinato
  * Licensed under MIT (https://github.com/svelto/svelto/blob/master/LICENSE)
  * =========================================================================
- * @before ./constants.js
- * @before ./debugger.js
- * @before ./find_matches.js
- * @before ./format.js
- * @before ./frames.js
- * @before ./mkize.js
- * @before ./move.js
- * @before ./now_secs.js
- * @before ./replace_all.js
- * @before ./round_closer.js
- * @before ./time_ago.js
- * @before ./wait.js
+ * @require ../init.js
  * ========================================================================= */
+
+(function ( _ ) {
+
+  'use strict';
+
+  /* DEBUGGER */
+
+  _.mixin ({
+
+    debugger () {
+
+      debugger;
+
+    }
+
+  });
+
+}( window.__svelto_lodash ));
