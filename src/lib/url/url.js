2d9031d05ba2689b2b1d0de54a623db3e144dcc5
Fabio Spampinato <spampinabio@gmail.com>
2018-01-26 01:38:14 +0100

    URL: added a `makeAbsolute` function

--- src/lib/url/url.js
+++ src/lib/url/url.js
@@ -21,6 +21,10 @@
       url1 = stripTarget ? url1.replace ( URL.targetRe, '' ) : url1;
       url2 = stripTarget ? url2.replace ( URL.targetRe, '' ) : url2;
       return _.trimEnd ( url1, '/' ) === _.trimEnd ( url2, '/' );
+    },
+    makeAbsolute ( url ) {
+      if ( url.startsWith ( '/' ) || url.includes ( '://' ) ) return url;
+      return `/${url}`;
     }
   };
 
