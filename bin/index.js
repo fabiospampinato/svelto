f7ec31b5bb3182e9a4ef438b59095608df36fe3c
Fabio Spampinato <spampinabio@gmail.com>
2018-02-26 22:39:59 +0100

    Added support for a `svelto.json` configuration file

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-//TODO: replace `pacco` to `svelto` in the help text
+//TODO: Replace `pacco` to `svelto` in the help text, we probably need to add an API for this to Caporal
 
 /* REQUIRE */
 
 const execa = require ( 'execa' ),
       path = require ( 'path' ),
+      rdf = require ( 'require-dot-file' ),
       config = require ( '../pacco.json' );
 
 /* VARIABLES */
@@ -13,11 +14,12 @@ const execa = require ( 'execa' ),
 const root = path.resolve ( __dirname, '..' ),
       src = path.join ( root, 'src' ),
       icon = path.join ( root, 'resources', 'icon', 'icon.png' ),
-      iconError = path.join ( root, 'resources', 'icon', 'icon_error.png' );
+      iconError = path.join ( root, 'resources', 'icon', 'icon_error.png' ),
+      sveltoConfig = rdf ( 'svelto.json', process.cwd () ) || {};
 
 /* EXECUTE */
 
-const args = ['--config', JSON.stringify ( config ), '--source', src, '--icon', icon, '--icon-error', iconError, ...process.argv.slice ( 2 )];
+const args = ['--config', JSON.stringify ( config ), '--config', JSON.stringify ( sveltoConfig ), '--source', src, '--icon', icon, '--icon-error', iconError, ...process.argv.slice ( 2 )];
 const opts = {
   cwd: process.cwd (),
   stdio: 'inherit'
