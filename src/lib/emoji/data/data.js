dd99d2b64c0d998c46e7de4f1abe235e930ebe30
Fabio Spampinato <spampinabio@gmail.com>
2018-03-22 00:56:40 +0100

    EmojiData: updated raw url

--- src/lib/emoji/data/data.js
+++ src/lib/emoji/data/data.js
@@ -21,7 +21,7 @@
     /* VARIABLES */
 
     _raw: EmojiDataRaw,
-    _rawUrl: `/json/emoji.json?v=${Svelto.VERSION}`,
+    _rawUrl: `/static/json/emoji.json?v=${Svelto.VERSION}`,
     _data: undefined,
 
     /* UTILITIES */
