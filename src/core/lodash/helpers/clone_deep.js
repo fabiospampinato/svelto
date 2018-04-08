aa8cc2c4cf60189cb3cfc4aac09f6d59cc7fb300
Fabio Spampinato <spampinabio@gmail.com>
2018-04-08 17:15:55 +0200

    _.cloneDeep: avoiding calling `clone` on the object

--- src/core/lodash/helpers/clone_deep.js
+++ src/core/lodash/helpers/clone_deep.js
@@ -10,7 +10,6 @@
 
     if ( obj === null || typeof ( obj ) !== 'object' || _.isElement ( obj ) || _.isRegExp ( obj ) || '__is_cloning__' in obj ) return obj;
 
-    if ( typeof obj.clone === 'function' ) return obj.clone (); // There are no guaranties this will always work
 
     const dupe = obj instanceof Date ? new obj.constructor () : obj.constructor ();
 
