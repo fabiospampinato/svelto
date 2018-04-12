de0604660061203a0d53dfcc2b7304fbae27613e
Fabio Spampinato <spampinabio@gmail.com>
2018-04-12 15:02:07 +0200

    $.makeEvent: added support for namespaces — assuming custom methods are provided by the dom lib

--- src/core/jquery/helpers/make_event.js
+++ src/core/jquery/helpers/make_event.js
@@ -4,44 +4,34 @@
 
 (function ( $, _ ) {
 
+  /* UTILTIES */
+
+  function parseEventName( fullName ) {
+    const parts = fullName.split ( '.' );
+    return [parts[0], parts.slice ( 1 ).join ( '.' )]; // [name, namespaces]
+  }
+
   /* MAKE EVENT */ // Creates an event by name
 
-  $.makeEvent = function ( name, originalEvent ) {
+  $.makeEvent = function ( fullName, originalEvent ) {
 
-    let event;
+    let [name, namespaces] = parseEventName ( fullName ),
+        event;
 
     if ( $.Event ) {
 
       event = new $.Event ( originalEvent || name );
       event.type = name;
 
-    } else { // Adding jQuery-like methods
+    } else {
 
       event = document.createEvent ( 'HTMLEvents' );
       event.initEvent ( name, true, false );
-
-      event.isDefaultPrevented = function () {
-        return this.defaultPrevented;
-      };
-
-      event.isPropagationStopped = _.false;
-      const stopPropagation = event.stopPropagation;
-      event.stopPropagation = function () {
-        event.isPropagationStopped = _.true;
-        return stopPropagation.call ( this );
-      };
-
-      event.isImmediatePropagationStopped = _.false;
-      const stopImmediatePropagation = event.stopImmediatePropagation;
-      event.stopImmediatePropagation = function () {
-        event.isPropagationStopped = _.true;
-        event.isImmediatePropagationStopped = _.true;
-        return stopImmediatePropagation.call ( this );
-      };
-
       event.originalEvent = originalEvent;
 
     }
+
+    event.namespace = namespaces;
 
     return event;
 
