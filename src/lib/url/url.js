4a61e04bd2ac6392f19d5cc6a5f08e1250d17b9f
Fabio Spampinato <spampinabio@gmail.com>
2018-05-10 23:18:36 +0200

    Avoiding using `_.trimEnd`

--- src/lib/url/url.js
+++ src/lib/url/url.js
@@ -6,12 +6,13 @@
   /* URL */
 
   let URL = {
+    trailingSlashRe: /\/$/,
     targetRe: /#(.*)$/,
     isEqual ( url1, url2, stripTarget = false ) {
       if ( !_.isString ( url1 ) || !_.isString ( url2 ) ) return url1 === url2;
       url1 = stripTarget ? url1.replace ( URL.targetRe, '' ) : url1;
       url2 = stripTarget ? url2.replace ( URL.targetRe, '' ) : url2;
-      return _.trimEnd ( url1, '/' ) === _.trimEnd ( url2, '/' );
+      return url1.replace ( URL.trailingSlashRe, '' ) === url2.replace ( URL.trailingSlashRe, '' );
     },
     makeAbsolute ( url ) {
       if ( url.startsWith ( '/' ) || url.includes ( '://' ) ) return url;
