b2d9d229224963003db168515bcc2b15e2421804
Fabio Spampinato <spampinabio@gmail.com>
2018-06-02 18:14:46 +0200

    Added a `ready` class to `html` once ready

---
+++ src/core/ready/ready.js
@@ -0,0 +1,11 @@
+
+// @priority -1000000000
+// @require core/modernizr/modernizr.js
+
+(function ( Modernizr ) {
+
+  /* READY */
+
+  Modernizr.addTest ( 'ready', true );
+
+}( window.Modernizr ));
