d70b23454e4634e075f633a715e14f18db494b7a
Fabio Spampinato <spampinabio@gmail.com>
2018-06-10 22:52:52 +0200

    $.isVisible: added support for checking if it’s visible in the viewport

--- src/core/jquery/helpers/is_visible.js
+++ src/core/jquery/helpers/is_visible.js
@@ -1,19 +1,31 @@
 
 // @require ../init.js
+// @require ./get_rect.js
 
 (function ( $ ) {
 
   /* IS VISIBLE */
 
-  $.isVisible = function ( ele ) {
+  $.isVisible = function ( ele, inViewport ) {
 
-    return !!ele && !!( ele.offsetWidth || ele.offsetHeight || ele.getClientRects ().length );
+    if ( !ele || !( ele.offsetWidth || ele.offsetHeight || ele.getClientRects ().length ) ) return false;
+
+    if ( inViewport ) {
+
+      const rect1 = $.getRect ( ele ),
+            rect2 = $.getWindowRect ();
+
+      return !( rect2.left > rect1.right || rect2.right < rect1.left || rect2.top > rect1.bottom || rect2.bottom < rect1.top );
+
+    }
+
+    return true;
 
   };
 
-  $.fn.isVisible = function () {
+  $.fn.isVisible = function ( inViewport ) {
 
-    return $.isVisible ( this[0] );
+    return $.isVisible ( this[0], inViewport );
 
   };
 
